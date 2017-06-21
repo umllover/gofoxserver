2d40a4c9635e0010a97da7885a60d7112b7417ee
jiazhen <497216192@qq.com>
2017-06-21 11:58:51 +0800

    跑马灯流程

--- hallServer/raceMsg/internal/handler.go
+++ hallServer/raceMsg/internal/handler.go
@@ -1,9 +1,17 @@
 package internal
 
 import (
-	"mj/common/msg"
+	"math/rand"
 	"reflect"
+	"time"
 
+	"mj/common/msg"
+	"mj/hallServer/user"
+	"mj/hallServer/userHandle"
+
+	"mj/hallServer/db/model"
+
+	"github.com/lovelly/leaf/log"
 )
 
 ////注册rpc 消息
@@ -21,12 +29,63 @@ func init() {
 
 }
 
-//发送给房间所有人
-func SendChatMsgToAll(args []interface{}) {
+// 接收到消息，存表
+func ReciveMsg(beginTime int, endTime int, interval int, context string) {
 
+	var raceMsginfo model.RaceMsgInfo
+	raceMsginfo.Context = context
+	raceMsginfo.EndTime = endTime
+	raceMsginfo.StartTime = beginTime
+	raceMsginfo.IntervalTime = interval
+
+	log.Debug("准备插入数据库")
+	lastId, rerror := model.RaceMsgInfoOp.Insert(&raceMsginfo)
+	if rerror != nil {
+		log.Debug("插入失败")
+	}
+	log.Debug("插入数据库结果:", lastId)
 }
 
-//发送给房间某人
-func sendCharMsgToUser(args []interface{}) {
+// 启动跑马灯
+func StartHorseRaceLamp() {
+	go startTimer(5 * 60)
+}
+
+// 启动定时器
+func startTimer(nTime int) {
+	log.Debug("定时器启动了")
+	timerMsg := time.NewTicker(time.Duration(nTime) * time.Second)
+	for {
+		select {
+		case <-timerMsg.C:
+			log.Debug("定时器时间到了，开始发消息")
+			msgInfo, err := model.RaceMsgInfoOp.SelectAll()
+			if err != nil {
+				log.Error("race_msg_info查找所有数据失败,error=%i", err)
+				break
+			}
+
+			msgCount := len(msgInfo)
+			r := rand.New(rand.NewSource(time.Now().UnixNano()))
+			index := r.Intn(msgCount)
+
+			//timerMsg.Stop()
+			SendMsgToAll(msgInfo[index].Context)
+		default:
+		}
+	}
+}
+
+//发送消息给所有人
+func SendMsgToAll(data interface{}) {
+
+	log.Debug("即将发送消息给所有人：%v", data)
+	userHandle.UserMgr.ForEachUser(func(u *user.User) {
+		u.WriteMsg(data)
+	})
+}
+
+//发送消息给某人
+func sendMsgToUser(args []interface{}) {
 
 }
