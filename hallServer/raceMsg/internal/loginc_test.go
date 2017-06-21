f941bbff3a3bb246f66676124054170b038ac326
jiazhen <497216192@qq.com>
2017-06-21 11:58:51 +0800

    跑马灯流程

---
+++ hallServer/raceMsg/internal/loginc_test.go
@@ -0,0 +1,24 @@
+package internal
+
+import (
+	"testing"
+)
+
+func TestSendMsgToAll(t *testing.T) {
+	//startTimer(0)
+	reciveMsg(1, 2, 2, "dfsd")
+}
+
+//func init()  {
+//	conf.init()
+//	lconf.LogLevel = conf.Server.LogLevel
+//	lconf.LogPath = conf.Server.LogPath
+//	lconf.LogFlag = conf.LogFlag
+//	lconf.ServerName = conf.ServerName()
+//	lconf.ConsolePort = conf.Server.ConsolePort
+//	lconf.ProfilePath = conf.Server.ProfilePath
+//	lconf.ListenAddr = conf.Server.ListenAddr
+//	lconf.ConnAddrs = conf.Server.ConnAddrs
+//	lconf.PendingWriteNum = conf.Server.PendingWriteNum
+//	lconf.HeartBeatInterval = conf.HeartBeatInterval
+//}
