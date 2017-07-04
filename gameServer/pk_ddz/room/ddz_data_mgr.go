d8d98552b731d2caaa888acc435ac65dd714b864
jiazhen <497216192@qq.com>
2017-07-04 16:45:24 +0800

    完善斗地主流程

--- gameServer/pk_ddz/room/ddz_data_mgr.go
+++ gameServer/pk_ddz/room/ddz_data_mgr.go
@@ -13,4 +13,44 @@ func NewDataMgr(id, uid, ConfigIdx int, name string, temp *base.GameServiceOptio
 
 type ddz_data_mgr struct {
 	*pk_base.RoomData
+	CurrentUser  int              // 当前玩家
+	OutCardCount [GAME_PLAYER]int // 出牌次数
+	WaitTime     int              // 等待时间
+	TimerControl int              // 时间控制
+
+	// 托管信息
+	OffLineTrustee bool // 离线托管
+
+	// 炸弹信息
+	BombCount     int              // 炸弹个数
+	EachBombCount [GAME_PLAYER]int // 炸弹个数
+
+	// 叫分信息
+	CallScoreCount int              // 叫分次数
+	BankerScore    int              // 庄家叫分
+	ScoreInfo      [GAME_PLAYER]int // 叫分信息
+
+	// 出牌信息
+	TurnWiner     int            // 胜利玩家
+	TurnCardCount int            // 出牌数目
+	TurnCardData  [MAX_COUNT]int // 出牌数据
+
+	// 扑克信息
+	BankerCard    [3]int                      // 游戏底牌
+	HandCardCount [GAME_PLAYER]int            // 扑克数目
+	HandCardData  [GAME_PLAYER][MAX_COUNT]int // 手上扑克
+
+	// 组件变量
+	//CGameLogic						m_GameLogic;						//游戏逻辑
+
+	// 组件接口
+	//ITableFrame	*					m_pITableFrame;						//框架接口
+	//tagCustomRule *					m_pGameCustomRule;					//自定规则
+	//tagGameServiceOption *			m_pGameServiceOption;				//游戏配置
+	//tagGameServiceAttrib *			m_pGameServiceAttrib;				//游戏属性
+}
+
+// 游戏开始
+func (room *DDZ_Entry) OnEventGameStart() {
+
 }
