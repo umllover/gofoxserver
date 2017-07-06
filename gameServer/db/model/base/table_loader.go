7245785d6e24bf9951584fd98f83b6a346e090a6
zdq <9682950@qq.com>
2017-07-06 16:10:47 +0800

    Merge branch 'develop' of 192.168.1.78:root/mq_server into refactor_logic
    # Conflicts:
    #	gameServer/common/pk/pk_base/pk_base_logic.go

--- gameServer/db/model/base/table_loader.go
+++ gameServer/db/model/base/table_loader.go
@@ -5,28 +5,26 @@ package base
 
 import (
 	"mj/gameServer/db"
-	"time"
-
-	"github.com/lovelly/leaf/log"
+    "github.com/lovelly/leaf/log"
 )
 
 func LoadBaseData() {
 	var start = time.Now().UnixNano()
-	ActivityCache.LoadAll()
-	GameServiceOptionCache.LoadAll()
-	GameTestpaiCache.LoadAll()
-	GlobalVarCache.LoadAll()
-	GoodsCache.LoadAll()
-	PersonalTableFeeCache.LoadAll()
-	RefreshInTimeCache.LoadAll()
-	ServerListCache.LoadAll()
-	db.BaseDataCaches["Activity"] = ActivityCache
-	db.BaseDataCaches["GameServiceOption"] = GameServiceOptionCache
-	db.BaseDataCaches["GameTestpai"] = GameTestpaiCache
-	db.BaseDataCaches["GlobalVar"] = GlobalVarCache
-	db.BaseDataCaches["Goods"] = GoodsCache
-	db.BaseDataCaches["PersonalTableFee"] = PersonalTableFeeCache
-	db.BaseDataCaches["RefreshInTime"] = RefreshInTimeCache
-	db.BaseDataCaches["ServerList"] = ServerListCache
-	log.Debug("loadBaseData %v, %v %v", 8, time.Now().UnixNano()-start, "ns")
+    ActivityCache.LoadAll()
+    GameServiceOptionCache.LoadAll()
+    GameTestpaiCache.LoadAll()
+    GlobalVarCache.LoadAll()
+    GoodsCache.LoadAll()
+    PersonalTableFeeCache.LoadAll()
+    RefreshInTimeCache.LoadAll()
+    ServerListCache.LoadAll()
+    db.BaseDataCaches["Activity"] = ActivityCache
+    db.BaseDataCaches["GameServiceOption"] = GameServiceOptionCache
+    db.BaseDataCaches["GameTestpai"] = GameTestpaiCache
+    db.BaseDataCaches["GlobalVar"] = GlobalVarCache
+    db.BaseDataCaches["Goods"] = GoodsCache
+    db.BaseDataCaches["PersonalTableFee"] = PersonalTableFeeCache
+    db.BaseDataCaches["RefreshInTime"] = RefreshInTimeCache
+    db.BaseDataCaches["ServerList"] = ServerListCache
+    log.Debug("loadBaseData %v, %v %v", 8,  time.Now().UnixNano()-start, "ns")
 }
