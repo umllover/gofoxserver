643b253f35cd98148cdd95bee72d9f203296441c
zdq <9682950@qq.com>
2017-07-06 16:10:47 +0800

    Merge branch 'develop' of 192.168.1.78:root/mq_server into refactor_logic
    # Conflicts:
    #	gameServer/common/pk/pk_base/pk_base_logic.go

--- gameServer/db/model/base/game_testpai.go
+++ gameServer/db/model/base/game_testpai.go
@@ -1,9 +1,11 @@
 package base
 
-import (
-	"mj/gameServer/db"
-
-	"github.com/lovelly/leaf/log"
+import(
+    "mj/gameServer/db"
+    "github.com/lovelly/leaf/log"
+    "github.com/jmoiron/sqlx"
+    "fmt"
+    "strings"
 )
 
 //This file is generate by scripts,don't edit it
@@ -11,53 +13,53 @@ import (
 //game_testpai
 //
 
-// +gen
+// +gen 
 type GameTestpai struct {
-	Id        int    `db:"id" json:"id"`               //
-	KindID    int    `db:"KindID" json:"KindID"`       // 游戏第一类型
-	ServerID  int    `db:"ServerID" json:"ServerID"`   // 游戏第二类型
-	CardsName string `db:"CardsName" json:"CardsName"` // 卡牌类型名称
-	ChairId   int    `db:"chair_id" json:"chair_id"`   // 座位id
-	Cards     string `db:"Cards" json:"Cards"`         // 所有玩家卡牌
-	IsAcivate int8   `db:"IsAcivate" json:"IsAcivate"` // 是否激活牌型
-}
+    Id int `db:"id" json:"id"` // 
+    KindID int `db:"KindID" json:"KindID"` // 游戏第一类型
+    ServerID int `db:"ServerID" json:"ServerID"` // 游戏第二类型
+    CardsName string `db:"CardsName" json:"CardsName"` // 卡牌类型名称
+    ChairId string `db:"chair_id" json:"chair_id"` // 座位id
+    Cards string `db:"Cards" json:"Cards"` // 所有玩家卡牌
+    IsAcivate int8 `db:"IsAcivate" json:"IsAcivate"` // 是否激活牌型
+    }
 
 var DefaultGameTestpai = GameTestpai{}
-
-type gameTestpaiCache struct {
-	objMap  map[int]*GameTestpai
-	objList []*GameTestpai
+type gameTestpaiCache struct{
+    objMap map[int]*GameTestpai
+    objList []*GameTestpai
 }
 
 var GameTestpaiCache = &gameTestpaiCache{}
 
 func (c *gameTestpaiCache) LoadAll() {
-	sql := "select * from game_testpai"
+    sql := "select * from game_testpai"
 	c.objList = make([]*GameTestpai, 0)
-	err := db.BaseDB.Select(&c.objList, sql)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	c.objMap = make(map[int]*GameTestpai)
+    err := db.BaseDB.Select(&c.objList,sql)
+    if err != nil{
+        log.Fatal(err.Error())
+    }
+    c.objMap = make(map[int]*GameTestpai)
 	log.Debug("Load all game_testpai success %v", len(c.objList))
-	for _, v := range c.objList {
-		c.objMap[v.Id] = v
-	}
+    for _,v := range c.objList{
+        c.objMap[v.Id] = v
+		}
 }
 
-func (c *gameTestpaiCache) All() []*GameTestpai {
-	return c.objList
+func (c *gameTestpaiCache) All() []*GameTestpai{
+    return c.objList
 }
 
 func (c *gameTestpaiCache) Count() int {
-	return len(c.objList)
+    return len(c.objList)
 }
 
-func (c *gameTestpaiCache) Get(id int) (*GameTestpai, bool) {
-	return c.GetKey1(id)
+func (c *gameTestpaiCache) Get(id int) (*GameTestpai, bool){
+    return c.GetKey1(id)
+	}
+
+func (c *gameTestpaiCache) GetKey1(id int) (*GameTestpai, bool){
+    v,ok :=  c.objMap[id]
+    return v,ok
 }
 
-func (c *gameTestpaiCache) GetKey1(id int) (*GameTestpai, bool) {
-	v, ok := c.objMap[id]
-	return v, ok
-}
