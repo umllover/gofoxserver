1deabd5fae59c6622dc64321794f81a6455219bf
zdq <9682950@qq.com>
2017-07-06 16:10:47 +0800

    Merge branch 'develop' of 192.168.1.78:root/mq_server into refactor_logic
    # Conflicts:
    #	gameServer/common/pk/pk_base/pk_base_logic.go

--- gameServer/db/model/accountsinfo.go
+++ gameServer/db/model/accountsinfo.go
@@ -1,13 +1,11 @@
 package model
 
-import (
-	"errors"
-	"fmt"
-	"mj/gameServer/db"
-	"time"
-
-	"github.com/jmoiron/sqlx"
-	"github.com/lovelly/leaf/log"
+import(
+    "mj/gameServer/db"
+    "github.com/lovelly/leaf/log"
+    "github.com/jmoiron/sqlx"
+    "fmt"
+    "strings"
 )
 
 //This file is generate by scripts,don't edit it
@@ -17,65 +15,64 @@ import (
 
 // +gen *
 type Accountsinfo struct {
-	UserID           int        `db:"UserID" json:"UserID"`                     // 用户标识
-	ProtectID        int        `db:"ProtectID" json:"ProtectID"`               // 密保标识
-	SpreaderID       int        `db:"SpreaderID" json:"SpreaderID"`             // 推广员标识
-	Accounts         string     `db:"Accounts" json:"Accounts"`                 // 用户帐号
-	NickName         string     `db:"NickName" json:"NickName"`                 // 用户昵称
-	PassPortID       string     `db:"PassPortID" json:"PassPortID"`             // 身份证号
-	Compellation     string     `db:"Compellation" json:"Compellation"`         // 真实名字
-	LogonPass        string     `db:"LogonPass" json:"LogonPass"`               // 登录密码
-	IsAndroid        int8       `db:"IsAndroid" json:"IsAndroid"`               //
-	InsurePass       string     `db:"InsurePass" json:"InsurePass"`             // 安全密码
-	MasterOrder      int8       `db:"MasterOrder" json:"MasterOrder"`           // 管理等级
-	Gender           int8       `db:"Gender" json:"Gender"`                     // 用户性别
-	Nullity          int8       `db:"Nullity" json:"Nullity"`                   // 禁止服务
-	NullityOverDate  *time.Time `db:"NullityOverDate" json:"NullityOverDate"`   // 禁止时间
-	StunDown         int8       `db:"StunDown" json:"StunDown"`                 // 关闭标志
-	MoorMachine      int8       `db:"MoorMachine" json:"MoorMachine"`           // 固定机器
-	WebLogonTimes    int        `db:"WebLogonTimes" json:"WebLogonTimes"`       // 登录次数
-	GameLogonTimes   int        `db:"GameLogonTimes" json:"GameLogonTimes"`     // 登录次数
-	LastLogonIP      string     `db:"LastLogonIP" json:"LastLogonIP"`           // 登录地址
-	LastLogonDate    *time.Time `db:"LastLogonDate" json:"LastLogonDate"`       // 登录时间
-	LastLogonMobile  string     `db:"LastLogonMobile" json:"LastLogonMobile"`   // 登录手机
-	LastLogonMachine string     `db:"LastLogonMachine" json:"LastLogonMachine"` // 登录机器
-	RegisterIP       string     `db:"RegisterIP" json:"RegisterIP"`             // 注册地址
-	RegisterDate     *time.Time `db:"RegisterDate" json:"RegisterDate"`         // 注册时间
-	RegisterMobile   string     `db:"RegisterMobile" json:"RegisterMobile"`     // 注册手机
-	RegisterMachine  string     `db:"RegisterMachine" json:"RegisterMachine"`   // 注册机器
-	QQID             string     `db:"QQID" json:"QQID"`                         // QQ对应ID
-	WXID             string     `db:"WXID" json:"WXID"`                         // 微信对应ID
-	AgentID          int        `db:"AgentID" json:"AgentID"`                   //
-	AgentNumber      string     `db:"AgentNumber" json:"AgentNumber"`           //
-	HeadImgUrl       string     `db:"HeadImgUrl" json:"HeadImgUrl"`             //
-	UnionID          string     `db:"UnionID" json:"UnionID"`                   //
-	QQ               string     `db:"QQ" json:"QQ"`                             // QQ 号码
-	EMail            string     `db:"EMail" json:"EMail"`                       //
-	DwellingPlace    string     `db:"DwellingPlace" json:"DwellingPlace"`       // 详细住址
-	PostalCode       string     `db:"PostalCode" json:"PostalCode"`             // 邮政编码
-	Birthday         *time.Time `db:"Birthday" json:"Birthday"`                 // 生日
-}
+    UserID int `db:"UserID" json:"UserID"` // 用户标识
+    ProtectID int `db:"ProtectID" json:"ProtectID"` // 密保标识
+    SpreaderID int `db:"SpreaderID" json:"SpreaderID"` // 推广员标识
+    Accounts string `db:"Accounts" json:"Accounts"` // 用户帐号
+    NickName string `db:"NickName" json:"NickName"` // 用户昵称
+    PassPortID string `db:"PassPortID" json:"PassPortID"` // 身份证号
+    Compellation string `db:"Compellation" json:"Compellation"` // 真实名字
+    LogonPass string `db:"LogonPass" json:"LogonPass"` // 登录密码
+    IsAndroid int8 `db:"IsAndroid" json:"IsAndroid"` // 
+    InsurePass string `db:"InsurePass" json:"InsurePass"` // 安全密码
+    MasterOrder int8 `db:"MasterOrder" json:"MasterOrder"` // 管理等级
+    Gender int8 `db:"Gender" json:"Gender"` // 用户性别
+    Nullity int8 `db:"Nullity" json:"Nullity"` // 禁止服务
+    NullityOverDate *time.Time `db:"NullityOverDate" json:"NullityOverDate"` // 禁止时间
+    StunDown int8 `db:"StunDown" json:"StunDown"` // 关闭标志
+    MoorMachine int8 `db:"MoorMachine" json:"MoorMachine"` // 固定机器
+    WebLogonTimes int `db:"WebLogonTimes" json:"WebLogonTimes"` // 登录次数
+    GameLogonTimes int `db:"GameLogonTimes" json:"GameLogonTimes"` // 登录次数
+    LastLogonIP string `db:"LastLogonIP" json:"LastLogonIP"` // 登录地址
+    LastLogonDate *time.Time `db:"LastLogonDate" json:"LastLogonDate"` // 登录时间
+    LastLogonMobile string `db:"LastLogonMobile" json:"LastLogonMobile"` // 登录手机
+    LastLogonMachine string `db:"LastLogonMachine" json:"LastLogonMachine"` // 登录机器
+    RegisterIP string `db:"RegisterIP" json:"RegisterIP"` // 注册地址
+    RegisterDate *time.Time `db:"RegisterDate" json:"RegisterDate"` // 注册时间
+    RegisterMobile string `db:"RegisterMobile" json:"RegisterMobile"` // 注册手机
+    RegisterMachine string `db:"RegisterMachine" json:"RegisterMachine"` // 注册机器
+    QQID string `db:"QQID" json:"QQID"` // QQ对应ID
+    WXID string `db:"WXID" json:"WXID"` // 微信对应ID
+    AgentID int `db:"AgentID" json:"AgentID"` // 
+    AgentNumber string `db:"AgentNumber" json:"AgentNumber"` // 
+    HeadImgUrl string `db:"HeadImgUrl" json:"HeadImgUrl"` // 
+    UnionID string `db:"UnionID" json:"UnionID"` // 
+    QQ string `db:"QQ" json:"QQ"` // QQ 号码
+    EMail string `db:"EMail" json:"EMail"` // 
+    DwellingPlace string `db:"DwellingPlace" json:"DwellingPlace"` // 详细住址
+    PostalCode string `db:"PostalCode" json:"PostalCode"` // 邮政编码
+    Birthday *time.Time `db:"Birthday" json:"Birthday"` // 生日
+    }
 
 type accountsinfoOp struct{}
 
 var AccountsinfoOp = &accountsinfoOp{}
 var DefaultAccountsinfo = &Accountsinfo{}
-
 // 按主键查询. 注:未找到记录的话将触发sql.ErrNoRows错误，返回nil, false
 func (op *accountsinfoOp) Get(UserID int) (*Accountsinfo, bool) {
-	obj := &Accountsinfo{}
-	sql := "select * from accountsinfo where UserID=? "
-	err := db.DB.Get(obj, sql,
-		UserID,
-	)
-
-	if err != nil {
-		log.Error("Get data error:%v", err.Error())
-		return nil, false
-	}
-	return obj, true
-}
-func (op *accountsinfoOp) SelectAll() ([]*Accountsinfo, error) {
+    obj := &Accountsinfo{}
+    sql := "select * from accountsinfo where UserID=? "
+    err := db.DB.Get(obj, sql, 
+        UserID,
+        )
+    
+    if err != nil{
+        log.Error("Get data error:%v", err.Error())
+        return nil,false
+    }
+    return obj, true
+} 
+func(op *accountsinfoOp) SelectAll() ([]*Accountsinfo, error) {
 	objList := []*Accountsinfo{}
 	sql := "select * from accountsinfo "
 	err := db.DB.Select(&objList, sql)
@@ -86,15 +83,15 @@ func (op *accountsinfoOp) SelectAll() ([]*Accountsinfo, error) {
 	return objList, nil
 }
 
-func (op *accountsinfoOp) QueryByMap(m map[string]interface{}) ([]*Accountsinfo, error) {
+func(op *accountsinfoOp) QueryByMap(m map[string]interface{}) ([]*Accountsinfo, error) {
 	result := []*Accountsinfo{}
-	var params []interface{}
+    var params []interface{}
 
 	sql := "select * from accountsinfo where 1=1 "
-	for k, v := range m {
-		sql += fmt.Sprintf(" and %s=? ", k)
-		params = append(params, v)
-	}
+    for k, v := range m{
+        sql += fmt.Sprintf(" and %s=? ", k)
+        params = append(params, v)
+    }
 	err := db.DB.Select(&result, sql, params...)
 	if err != nil {
 		log.Error(err.Error())
@@ -103,15 +100,16 @@ func (op *accountsinfoOp) QueryByMap(m map[string]interface{}) ([]*Accountsinfo,
 	return result, nil
 }
 
-func (op *accountsinfoOp) GetByMap(m map[string]interface{}) (*Accountsinfo, error) {
-	lst, err := op.QueryByMap(m)
-	if err != nil {
-		return nil, err
-	}
-	if len(lst) > 0 {
-		return lst[0], nil
-	}
-	return nil, errors.New("no row in result")
+
+func(op *accountsinfoOp) GetByMap(m map[string]interface{}) (*Accountsinfo, error) {
+    lst, err := op.QueryByMap(m)
+    if err != nil {
+        return nil, err
+    }
+    if len(lst) > 0 {
+        return lst[0], nil
+    }
+    return nil, errors.New("no row in result")
 }
 
 /*
@@ -126,57 +124,57 @@ func (i *Accountsinfo) Insert() error {
 
 // 插入数据，自增长字段将被忽略
 func (op *accountsinfoOp) Insert(m *Accountsinfo) (int64, error) {
-	return op.InsertTx(db.DB, m)
+    return op.InsertTx(db.DB, m)
 }
 
 // 插入数据，自增长字段将被忽略
 func (op *accountsinfoOp) InsertTx(ext sqlx.Ext, m *Accountsinfo) (int64, error) {
-	sql := "insert into accountsinfo(ProtectID,SpreaderID,Accounts,NickName,PassPortID,Compellation,LogonPass,IsAndroid,InsurePass,MasterOrder,Gender,Nullity,NullityOverDate,StunDown,MoorMachine,WebLogonTimes,GameLogonTimes,LastLogonIP,LastLogonDate,LastLogonMobile,LastLogonMachine,RegisterIP,RegisterDate,RegisterMobile,RegisterMachine,QQID,WXID,AgentID,AgentNumber,HeadImgUrl,UnionID,QQ,EMail,DwellingPlace,PostalCode,Birthday) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
-	result, err := ext.Exec(sql,
-		m.ProtectID,
-		m.SpreaderID,
-		m.Accounts,
-		m.NickName,
-		m.PassPortID,
-		m.Compellation,
-		m.LogonPass,
-		m.IsAndroid,
-		m.InsurePass,
-		m.MasterOrder,
-		m.Gender,
-		m.Nullity,
-		m.NullityOverDate,
-		m.StunDown,
-		m.MoorMachine,
-		m.WebLogonTimes,
-		m.GameLogonTimes,
-		m.LastLogonIP,
-		m.LastLogonDate,
-		m.LastLogonMobile,
-		m.LastLogonMachine,
-		m.RegisterIP,
-		m.RegisterDate,
-		m.RegisterMobile,
-		m.RegisterMachine,
-		m.QQID,
-		m.WXID,
-		m.AgentID,
-		m.AgentNumber,
-		m.HeadImgUrl,
-		m.UnionID,
-		m.QQ,
-		m.EMail,
-		m.DwellingPlace,
-		m.PostalCode,
-		m.Birthday,
-	)
-	if err != nil {
-		log.Error("InsertTx sql error:%v, data:%v", err.Error(), m)
-		return -1, err
-	}
-	affected, _ := result.LastInsertId()
-	return affected, nil
-}
+    sql := "insert into accountsinfo(ProtectID,SpreaderID,Accounts,NickName,PassPortID,Compellation,LogonPass,IsAndroid,InsurePass,MasterOrder,Gender,Nullity,NullityOverDate,StunDown,MoorMachine,WebLogonTimes,GameLogonTimes,LastLogonIP,LastLogonDate,LastLogonMobile,LastLogonMachine,RegisterIP,RegisterDate,RegisterMobile,RegisterMachine,QQID,WXID,AgentID,AgentNumber,HeadImgUrl,UnionID,QQ,EMail,DwellingPlace,PostalCode,Birthday) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
+    result, err := ext.Exec(sql,
+    m.ProtectID,
+        m.SpreaderID,
+        m.Accounts,
+        m.NickName,
+        m.PassPortID,
+        m.Compellation,
+        m.LogonPass,
+        m.IsAndroid,
+        m.InsurePass,
+        m.MasterOrder,
+        m.Gender,
+        m.Nullity,
+        m.NullityOverDate,
+        m.StunDown,
+        m.MoorMachine,
+        m.WebLogonTimes,
+        m.GameLogonTimes,
+        m.LastLogonIP,
+        m.LastLogonDate,
+        m.LastLogonMobile,
+        m.LastLogonMachine,
+        m.RegisterIP,
+        m.RegisterDate,
+        m.RegisterMobile,
+        m.RegisterMachine,
+        m.QQID,
+        m.WXID,
+        m.AgentID,
+        m.AgentNumber,
+        m.HeadImgUrl,
+        m.UnionID,
+        m.QQ,
+        m.EMail,
+        m.DwellingPlace,
+        m.PostalCode,
+        m.Birthday,
+        )
+    if err != nil{
+        log.Error("InsertTx sql error:%v, data:%v", err.Error(),m)
+        return -1, err
+    }
+    affected, _ := result.LastInsertId()
+        return affected, nil
+    }
 
 /*
 func (i *Accountsinfo) Update()  error {
@@ -189,83 +187,83 @@ func (i *Accountsinfo) Update()  error {
 */
 
 // 用主键(属性)做条件，更新除主键外的所有字段
-func (op *accountsinfoOp) Update(m *Accountsinfo) error {
-	return op.UpdateTx(db.DB, m)
+func (op *accountsinfoOp) Update(m *Accountsinfo) (error) {
+    return op.UpdateTx(db.DB, m)
 }
 
 // 用主键(属性)做条件，更新除主键外的所有字段
-func (op *accountsinfoOp) UpdateTx(ext sqlx.Ext, m *Accountsinfo) error {
-	sql := `update accountsinfo set ProtectID=?,SpreaderID=?,Accounts=?,NickName=?,PassPortID=?,Compellation=?,LogonPass=?,IsAndroid=?,InsurePass=?,MasterOrder=?,Gender=?,Nullity=?,NullityOverDate=?,StunDown=?,MoorMachine=?,WebLogonTimes=?,GameLogonTimes=?,LastLogonIP=?,LastLogonDate=?,LastLogonMobile=?,LastLogonMachine=?,RegisterIP=?,RegisterDate=?,RegisterMobile=?,RegisterMachine=?,QQID=?,WXID=?,AgentID=?,AgentNumber=?,HeadImgUrl=?,UnionID=?,QQ=?,EMail=?,DwellingPlace=?,PostalCode=?,Birthday=? where UserID=?`
-	_, err := ext.Exec(sql,
-		m.ProtectID,
-		m.SpreaderID,
-		m.Accounts,
-		m.NickName,
-		m.PassPortID,
-		m.Compellation,
-		m.LogonPass,
-		m.IsAndroid,
-		m.InsurePass,
-		m.MasterOrder,
-		m.Gender,
-		m.Nullity,
-		m.NullityOverDate,
-		m.StunDown,
-		m.MoorMachine,
-		m.WebLogonTimes,
-		m.GameLogonTimes,
-		m.LastLogonIP,
-		m.LastLogonDate,
-		m.LastLogonMobile,
-		m.LastLogonMachine,
-		m.RegisterIP,
-		m.RegisterDate,
-		m.RegisterMobile,
-		m.RegisterMachine,
-		m.QQID,
-		m.WXID,
-		m.AgentID,
-		m.AgentNumber,
-		m.HeadImgUrl,
-		m.UnionID,
-		m.QQ,
-		m.EMail,
-		m.DwellingPlace,
-		m.PostalCode,
-		m.Birthday,
-		m.UserID,
-	)
+func (op *accountsinfoOp) UpdateTx(ext sqlx.Ext, m *Accountsinfo) (error) {
+    sql := `update accountsinfo set ProtectID=?,SpreaderID=?,Accounts=?,NickName=?,PassPortID=?,Compellation=?,LogonPass=?,IsAndroid=?,InsurePass=?,MasterOrder=?,Gender=?,Nullity=?,NullityOverDate=?,StunDown=?,MoorMachine=?,WebLogonTimes=?,GameLogonTimes=?,LastLogonIP=?,LastLogonDate=?,LastLogonMobile=?,LastLogonMachine=?,RegisterIP=?,RegisterDate=?,RegisterMobile=?,RegisterMachine=?,QQID=?,WXID=?,AgentID=?,AgentNumber=?,HeadImgUrl=?,UnionID=?,QQ=?,EMail=?,DwellingPlace=?,PostalCode=?,Birthday=? where UserID=?`
+    _, err := ext.Exec(sql,
+    m.ProtectID,
+        m.SpreaderID,
+        m.Accounts,
+        m.NickName,
+        m.PassPortID,
+        m.Compellation,
+        m.LogonPass,
+        m.IsAndroid,
+        m.InsurePass,
+        m.MasterOrder,
+        m.Gender,
+        m.Nullity,
+        m.NullityOverDate,
+        m.StunDown,
+        m.MoorMachine,
+        m.WebLogonTimes,
+        m.GameLogonTimes,
+        m.LastLogonIP,
+        m.LastLogonDate,
+        m.LastLogonMobile,
+        m.LastLogonMachine,
+        m.RegisterIP,
+        m.RegisterDate,
+        m.RegisterMobile,
+        m.RegisterMachine,
+        m.QQID,
+        m.WXID,
+        m.AgentID,
+        m.AgentNumber,
+        m.HeadImgUrl,
+        m.UnionID,
+        m.QQ,
+        m.EMail,
+        m.DwellingPlace,
+        m.PostalCode,
+        m.Birthday,
+        m.UserID,
+        )
 
-	if err != nil {
-		log.Error("update sql error:%v, data:%v", err.Error(), m)
-		return err
-	}
+    if err != nil{
+		log.Error("update sql error:%v, data:%v", err.Error(),m)
+        return err
+    }
 
-	return nil
+    return nil
 }
 
 // 用主键做条件，更新map里包含的字段名
-func (op *accountsinfoOp) UpdateWithMap(UserID int, m map[string]interface{}) error {
-	return op.UpdateWithMapTx(db.DB, UserID, m)
+func (op *accountsinfoOp) UpdateWithMap(UserID int, m map[string]interface{}) (error) {
+    return op.UpdateWithMapTx(db.DB, UserID, m)
 }
 
 // 用主键做条件，更新map里包含的字段名
-func (op *accountsinfoOp) UpdateWithMapTx(ext sqlx.Ext, UserID int, m map[string]interface{}) error {
+func (op *accountsinfoOp) UpdateWithMapTx(ext sqlx.Ext, UserID int, m map[string]interface{}) (error) {
 
-	sql := `update accountsinfo set %s where 1=1 and UserID=? ;`
+    sql := `update accountsinfo set %s where 1=1 and UserID=? ;`
 
-	var params []interface{}
-	var set_sql string
-	for k, v := range m {
+    var params []interface{}
+    var set_sql string
+    for k, v := range m{
 		if set_sql != "" {
 			set_sql += ","
 		}
-		set_sql += fmt.Sprintf(" %s=? ", k)
-		params = append(params, v)
-	}
+        set_sql += fmt.Sprintf(" %s=? ", k)
+        params = append(params, v)
+    }
 	params = append(params, UserID)
-	_, err := ext.Exec(fmt.Sprintf(sql, set_sql), params...)
-	return err
+    _, err := ext.Exec(fmt.Sprintf(sql, set_sql), params...)
+    return err
 }
 
 /*
@@ -276,53 +274,54 @@ func (i *Accountsinfo) Delete() error{
 }
 */
 // 根据主键删除相关记录
-func (op *accountsinfoOp) Delete(UserID int) error {
-	return op.DeleteTx(db.DB, UserID)
+func (op *accountsinfoOp) Delete(UserID int) error{
+    return op.DeleteTx(db.DB, UserID)
 }
 
 // 根据主键删除相关记录,Tx
-func (op *accountsinfoOp) DeleteTx(ext sqlx.Ext, UserID int) error {
-	sql := `delete from accountsinfo where 1=1
+func (op *accountsinfoOp) DeleteTx(ext sqlx.Ext, UserID int) error{
+    sql := `delete from accountsinfo where 1=1
         and UserID=?
         `
-	_, err := ext.Exec(sql,
-		UserID,
-	)
-	return err
+    _, err := ext.Exec(sql, 
+        UserID,
+        )
+    return err
 }
 
 // 返回符合查询条件的记录数
 func (op *accountsinfoOp) CountByMap(m map[string]interface{}) (int64, error) {
 
-	var params []interface{}
-	sql := `select count(*) from accountsinfo where 1=1 `
-	for k, v := range m {
-		sql += fmt.Sprintf(" and  %s=? ", k)
-		params = append(params, v)
-	}
-	count := int64(-1)
-	err := db.DB.Get(&count, sql, params...)
-	if err != nil {
-		log.Error("CountByMap  error:%v data :%v", err.Error(), m)
-		return 0, err
-	}
-	return count, nil
+    var params []interface{}
+    sql := `select count(*) from accountsinfo where 1=1 `
+    for k, v := range m{
+        sql += fmt.Sprintf(" and  %s=? ",k)
+        params = append(params, v)
+    }
+    count := int64(-1)
+    err := db.DB.Get(&count, sql, params...)
+    if err != nil {
+        log.Error("CountByMap  error:%v data :%v", err.Error(), m)
+		return 0,err
+    }
+    return count, nil
 }
 
-func (op *accountsinfoOp) DeleteByMap(m map[string]interface{}) (int64, error) {
+func (op *accountsinfoOp) DeleteByMap(m map[string]interface{})(int64, error){
 	return op.DeleteByMapTx(db.DB, m)
 }
 
-func (op *accountsinfoOp) DeleteByMapTx(ext sqlx.Ext, m map[string]interface{}) (int64, error) {
+func (op *accountsinfoOp) DeleteByMapTx(ext sqlx.Ext, m map[string]interface{}) (int64, error){
 	var params []interface{}
 	sql := "delete from accountsinfo where 1=1 "
 	for k, v := range m {
 		sql += fmt.Sprintf(" and %s=? ", k)
 		params = append(params, v)
 	}
-	result, err := ext.Exec(sql, params...)
-	if err != nil {
-		return -1, err
-	}
-	return result.RowsAffected()
+	result, err := ext.Exec(sql, params...) 
+    if err != nil {
+        return -1, err
+    }
+    return result.RowsAffected()
 }
+
