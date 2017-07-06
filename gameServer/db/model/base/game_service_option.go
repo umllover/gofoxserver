3d3ab0827f086e8605c55e3214858a8b236a3b07
zdq <9682950@qq.com>
2017-07-06 16:10:47 +0800

    Merge branch 'develop' of 192.168.1.78:root/mq_server into refactor_logic
    # Conflicts:
    #	gameServer/common/pk/pk_base/pk_base_logic.go

--- gameServer/db/model/base/game_service_option.go
+++ gameServer/db/model/base/game_service_option.go
@@ -1,9 +1,11 @@
 package base
 
-import (
-	"mj/gameServer/db"
-
-	"github.com/lovelly/leaf/log"
+import(
+    "mj/gameServer/db"
+    "github.com/lovelly/leaf/log"
+    "github.com/jmoiron/sqlx"
+    "fmt"
+    "strings"
 )
 
 //This file is generate by scripts,don't edit it
@@ -11,84 +13,84 @@ import (
 //game_service_option
 //
 
-// +gen
+// +gen 
 type GameServiceOption struct {
-	KindID              int    `db:"KindID" json:"KindID"`                           // 名称号码
-	ServerID            int    `db:"ServerID" json:"ServerID"`                       // 房间标识
-	SortID              int    `db:"SortID" json:"SortID"`                           // 排列标识
-	CellScore           int    `db:"CellScore" json:"CellScore"`                     // 单位积分
-	AndroidMaxCellScore int    `db:"AndroidMaxCellScore" json:"AndroidMaxCellScore"` // 机器人最大进入底注
-	MinTableScore       int    `db:"MinTableScore" json:"MinTableScore"`             // 最少进入
-	MinEnterScore       int    `db:"MinEnterScore" json:"MinEnterScore"`             // 最低进入积分
-	MaxEnterScore       int    `db:"MaxEnterScore" json:"MaxEnterScore"`             // 最高积分
-	MinPlayer           int    `db:"MinPlayer" json:"MinPlayer"`                     // 最少几个人才能玩
-	MaxPlayer           int    `db:"MaxPlayer" json:"MaxPlayer"`                     // 最多多少人一起玩
-	ServerType          int    `db:"ServerType" json:"ServerType"`                   // 房间类型
-	RoomName            string `db:"RoomName" json:"RoomName"`                       // 房间名称
-	OffLineTrustee      int    `db:"OffLineTrustee" json:"OffLineTrustee"`           // 是否短线代打
-	CardOrBean          int8   `db:"CardOrBean" json:"CardOrBean"`                   // 消耗房卡还是游戏豆
-	FeeBeanOrRoomCard   int    `db:"FeeBeanOrRoomCard" json:"FeeBeanOrRoomCard"`     // 消耗房卡或游戏豆的数量
-	MaxCellScore        int    `db:"MaxCellScore" json:"MaxCellScore"`               // 房间最大底分
-	PlayTurnCount       int    `db:"PlayTurnCount" json:"PlayTurnCount"`             // 房间能够进行游戏的最大局数
-	TimeAfterBeginTime  int    `db:"TimeAfterBeginTime" json:"TimeAfterBeginTime"`   // 游戏开始后多长时间后解散桌子
-	TimeOffLineCount    int    `db:"TimeOffLineCount" json:"TimeOffLineCount"`       // 玩家掉线多长时间后解散桌子
-	TimeNotBeginGame    int    `db:"TimeNotBeginGame" json:"TimeNotBeginGame"`       // 多长时间未开始游戏解散桌子	 单位秒
-	DynamicJoin         int    `db:"DynamicJoin" json:"DynamicJoin"`                 // 是够允许游戏开始后加入 1是允许
-	OutCardTime         int    `db:"OutCardTime" json:"OutCardTime"`                 // 多久没出牌自动出牌
-	OperateCardTime     int    `db:"OperateCardTime" json:"OperateCardTime"`         // 操作最大时间
-}
+    KindID int `db:"KindID" json:"KindID"` // 名称号码
+    ServerID int `db:"ServerID" json:"ServerID"` // 房间标识
+    SortID int `db:"SortID" json:"SortID"` // 排列标识
+    CellScore int `db:"CellScore" json:"CellScore"` // 单位积分
+    AndroidMaxCellScore int `db:"AndroidMaxCellScore" json:"AndroidMaxCellScore"` // 机器人最大进入底注
+    MinTableScore int `db:"MinTableScore" json:"MinTableScore"` // 最少进入
+    MinEnterScore int `db:"MinEnterScore" json:"MinEnterScore"` // 最低进入积分
+    MaxEnterScore int `db:"MaxEnterScore" json:"MaxEnterScore"` // 最高积分
+    MinPlayer int `db:"MinPlayer" json:"MinPlayer"` // 最少几个人才能玩
+    MaxPlayer int `db:"MaxPlayer" json:"MaxPlayer"` // 最多多少人一起玩
+    ServerType int `db:"ServerType" json:"ServerType"` // 房间类型
+    RoomName string `db:"RoomName" json:"RoomName"` // 房间名称
+    OffLineTrustee int `db:"OffLineTrustee" json:"OffLineTrustee"` // 是否短线代打
+    CardOrBean int8 `db:"CardOrBean" json:"CardOrBean"` // 消耗房卡还是游戏豆
+    FeeBeanOrRoomCard int `db:"FeeBeanOrRoomCard" json:"FeeBeanOrRoomCard"` // 消耗房卡或游戏豆的数量
+    MaxCellScore int `db:"MaxCellScore" json:"MaxCellScore"` // 房间最大底分
+    PlayTurnCount int `db:"PlayTurnCount" json:"PlayTurnCount"` // 房间能够进行游戏的最大局数
+    TimeAfterBeginTime int `db:"TimeAfterBeginTime" json:"TimeAfterBeginTime"` // 游戏开始后多长时间后解散桌子
+    TimeOffLineCount int `db:"TimeOffLineCount" json:"TimeOffLineCount"` // 玩家掉线多长时间后解散桌子
+    TimeNotBeginGame int `db:"TimeNotBeginGame" json:"TimeNotBeginGame"` // 多长时间未开始游戏解散桌子	 单位秒
+    DynamicJoin int `db:"DynamicJoin" json:"DynamicJoin"` // 是够允许游戏开始后加入 1是允许
+    OutCardTime int `db:"OutCardTime" json:"OutCardTime"` // 多久没出牌自动出牌
+    OperateCardTime int `db:"OperateCardTime" json:"OperateCardTime"` // 操作最大时间
+    }
 
 var DefaultGameServiceOption = GameServiceOption{}
-
-type gameServiceOptionCache struct {
-	objMap  map[int]map[int]*GameServiceOption
+type gameServiceOptionCache struct{
+    objMap map[int]map[int]*GameServiceOption
 	objList []*GameServiceOption
 }
 
 var GameServiceOptionCache = &gameServiceOptionCache{}
 
 func (c *gameServiceOptionCache) LoadAll() {
-	sql := "select * from game_service_option"
+    sql := "select * from game_service_option"
 	c.objList = make([]*GameServiceOption, 0)
-	err := db.BaseDB.Select(&c.objList, sql)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	c.objMap = make(map[int]map[int]*GameServiceOption)
+    err := db.BaseDB.Select(&c.objList,sql)
+    if err != nil{
+        log.Fatal(err.Error())
+    }
+    c.objMap = make(map[int]map[int]*GameServiceOption)
 	log.Debug("Load all game_service_option success %v", len(c.objList))
-	for _, v := range c.objList {
-		obj, ok := c.objMap[v.KindID]
-		if !ok {
-			obj = make(map[int]*GameServiceOption)
-			c.objMap[v.KindID] = obj
+    for _,v := range c.objList{
+        obj, ok := c.objMap[v.KindID]
+			if !ok {
+				obj = make(map[int]*GameServiceOption)
+				c.objMap[v.KindID] = obj
+			}
+			obj[v.ServerID] = v
+			
 		}
-		obj[v.ServerID] = v
-
-	}
 }
 
-func (c *gameServiceOptionCache) All() []*GameServiceOption {
-	return c.objList
+func (c *gameServiceOptionCache) All() []*GameServiceOption{
+    return c.objList
 }
 
 func (c *gameServiceOptionCache) Count() int {
-	return len(c.objList)
+    return len(c.objList)
 }
 
-func (c *gameServiceOptionCache) Get(KindID int, ServerID int) (*GameServiceOption, bool) {
-	return c.GetKey2(KindID, ServerID)
+func (c *gameServiceOptionCache) Get(KindID int,ServerID int) (*GameServiceOption, bool){
+    return c.GetKey2(KindID,ServerID)
+	}
+
+func (c *gameServiceOptionCache) GetKey1(KindID int) (map[int]*GameServiceOption, bool){
+    v,ok :=  c.objMap[KindID]
+    return v,ok
 }
 
-func (c *gameServiceOptionCache) GetKey1(KindID int) (map[int]*GameServiceOption, bool) {
-	v, ok := c.objMap[KindID]
-	return v, ok
-}
-
-func (c *gameServiceOptionCache) GetKey2(KindID int, ServerID int) (*GameServiceOption, bool) {
-	v, ok := c.objMap[KindID]
+func (c *gameServiceOptionCache) GetKey2(KindID int,ServerID int) (*GameServiceOption, bool){
+    v,ok :=  c.objMap[KindID]
 	if !ok {
 		return nil, false
 	}
-	v1, ok1 := v[ServerID]
-	return v1, ok1
+	v1,ok1 :=  v[ServerID]
+	return v1,ok1
 }
+
