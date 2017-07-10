e488a8b38fa022226d7435dde755a642f9ca5f07
weilian <weilian@mq.com>
2017-07-10 10:22:49 +0800

    update

--- gameServer/db/model/gamescorelocker.go
+++ gameServer/db/model/gamescorelocker.go
@@ -1,11 +1,13 @@
 package model
 
-import(
-    "mj/gameServer/db"
-    "github.com/lovelly/leaf/log"
-    "github.com/jmoiron/sqlx"
-    "fmt"
-    "strings"
+import (
+	"errors"
+	"fmt"
+	"mj/gameServer/db"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/lovelly/leaf/log"
 )
 
 //This file is generate by scripts,don't edit it
@@ -15,36 +17,37 @@ import(
 
 // +gen *
 type Gamescorelocker struct {
-    UserID int `db:"UserID" json:"UserID"` // 用户索引
-    KindID int `db:"KindID" json:"KindID"` // 房间索引
-    ServerID int `db:"ServerID" json:"ServerID"` // 游戏标识
-    HallNodeID int `db:"HallNodeID" json:"HallNodeID"` // 
-    GameNodeID int `db:"GameNodeID" json:"GameNodeID"` // 在哪个服务器上
-    Roomid int `db:"roomid" json:"roomid"` // 进出索引
-    EnterIP string `db:"EnterIP" json:"EnterIP"` // 进入地址
-    EnterMachine string `db:"EnterMachine" json:"EnterMachine"` // 进入机器
-    CollectDate *time.Time `db:"CollectDate" json:"CollectDate"` // 录入日期
-    }
+	UserID       int        `db:"UserID" json:"UserID"`             // 用户索引
+	KindID       int        `db:"KindID" json:"KindID"`             // 房间索引
+	ServerID     int        `db:"ServerID" json:"ServerID"`         // 游戏标识
+	HallNodeID   int        `db:"HallNodeID" json:"HallNodeID"`     //
+	GameNodeID   int        `db:"GameNodeID" json:"GameNodeID"`     // 在哪个服务器上
+	Roomid       int        `db:"roomid" json:"roomid"`             // 进出索引
+	EnterIP      string     `db:"EnterIP" json:"EnterIP"`           // 进入地址
+	EnterMachine string     `db:"EnterMachine" json:"EnterMachine"` // 进入机器
+	CollectDate  *time.Time `db:"CollectDate" json:"CollectDate"`   // 录入日期
+}
 
 type gamescorelockerOp struct{}
 
 var GamescorelockerOp = &gamescorelockerOp{}
 var DefaultGamescorelocker = &Gamescorelocker{}
+
 // 按主键查询. 注:未找到记录的话将触发sql.ErrNoRows错误，返回nil, false
 func (op *gamescorelockerOp) Get(UserID int) (*Gamescorelocker, bool) {
-    obj := &Gamescorelocker{}
-    sql := "select * from gamescorelocker where UserID=? "
-    err := db.DB.Get(obj, sql, 
-        UserID,
-        )
-    
-    if err != nil{
-        log.Error("Get data error:%v", err.Error())
-        return nil,false
-    }
-    return obj, true
-} 
-func(op *gamescorelockerOp) SelectAll() ([]*Gamescorelocker, error) {
+	obj := &Gamescorelocker{}
+	sql := "select * from gamescorelocker where UserID=? "
+	err := db.DB.Get(obj, sql,
+		UserID,
+	)
+
+	if err != nil {
+		log.Error("Get data error:%v", err.Error())
+		return nil, false
+	}
+	return obj, true
+}
+func (op *gamescorelockerOp) SelectAll() ([]*Gamescorelocker, error) {
 	objList := []*Gamescorelocker{}
 	sql := "select * from gamescorelocker "
 	err := db.DB.Select(&objList, sql)
@@ -55,15 +58,15 @@ func(op *gamescorelockerOp) SelectAll() ([]*Gamescorelocker, error) {
 	return objList, nil
 }
 
-func(op *gamescorelockerOp) QueryByMap(m map[string]interface{}) ([]*Gamescorelocker, error) {
+func (op *gamescorelockerOp) QueryByMap(m map[string]interface{}) ([]*Gamescorelocker, error) {
 	result := []*Gamescorelocker{}
-    var params []interface{}
+	var params []interface{}
 
 	sql := "select * from gamescorelocker where 1=1 "
-    for k, v := range m{
-        sql += fmt.Sprintf(" and %s=? ", k)
-        params = append(params, v)
-    }
+	for k, v := range m {
+		sql += fmt.Sprintf(" and %s=? ", k)
+		params = append(params, v)
+	}
 	err := db.DB.Select(&result, sql, params...)
 	if err != nil {
 		log.Error(err.Error())
@@ -72,16 +75,15 @@ func(op *gamescorelockerOp) QueryByMap(m map[string]interface{}) ([]*Gamescorelo
 	return result, nil
 }
 
-
-func(op *gamescorelockerOp) GetByMap(m map[string]interface{}) (*Gamescorelocker, error) {
-    lst, err := op.QueryByMap(m)
-    if err != nil {
-        return nil, err
-    }
-    if len(lst) > 0 {
-        return lst[0], nil
-    }
-    return nil, errors.New("no row in result")
+func (op *gamescorelockerOp) GetByMap(m map[string]interface{}) (*Gamescorelocker, error) {
+	lst, err := op.QueryByMap(m)
+	if err != nil {
+		return nil, err
+	}
+	if len(lst) > 0 {
+		return lst[0], nil
+	}
+	return nil, errors.New("no row in result")
 }
 
 /*
@@ -96,30 +98,30 @@ func (i *Gamescorelocker) Insert() error {
 
 // 插入数据，自增长字段将被忽略
 func (op *gamescorelockerOp) Insert(m *Gamescorelocker) (int64, error) {
-    return op.InsertTx(db.DB, m)
+	return op.InsertTx(db.DB, m)
 }
 
 // 插入数据，自增长字段将被忽略
 func (op *gamescorelockerOp) InsertTx(ext sqlx.Ext, m *Gamescorelocker) (int64, error) {
-    sql := "insert into gamescorelocker(UserID,KindID,ServerID,HallNodeID,GameNodeID,roomid,EnterIP,EnterMachine,CollectDate) values(?,?,?,?,?,?,?,?,?)"
-    result, err := ext.Exec(sql,
-    m.UserID,
-        m.KindID,
-        m.ServerID,
-        m.HallNodeID,
-        m.GameNodeID,
-        m.Roomid,
-        m.EnterIP,
-        m.EnterMachine,
-        m.CollectDate,
-        )
-    if err != nil{
-        log.Error("InsertTx sql error:%v, data:%v", err.Error(),m)
-        return -1, err
-    }
-    affected, _ := result.LastInsertId()
-        return affected, nil
-    }
+	sql := "insert into gamescorelocker(UserID,KindID,ServerID,HallNodeID,GameNodeID,roomid,EnterIP,EnterMachine,CollectDate) values(?,?,?,?,?,?,?,?,?)"
+	result, err := ext.Exec(sql,
+		m.UserID,
+		m.KindID,
+		m.ServerID,
+		m.HallNodeID,
+		m.GameNodeID,
+		m.Roomid,
+		m.EnterIP,
+		m.EnterMachine,
+		m.CollectDate,
+	)
+	if err != nil {
+		log.Error("InsertTx sql error:%v, data:%v", err.Error(), m)
+		return -1, err
+	}
+	affected, _ := result.LastInsertId()
+	return affected, nil
+}
 
 /*
 func (i *Gamescorelocker) Update()  error {
@@ -132,55 +134,55 @@ func (i *Gamescorelocker) Update()  error {
 */
 
 // 用主键(属性)做条件，更新除主键外的所有字段
-func (op *gamescorelockerOp) Update(m *Gamescorelocker) (error) {
-    return op.UpdateTx(db.DB, m)
+func (op *gamescorelockerOp) Update(m *Gamescorelocker) error {
+	return op.UpdateTx(db.DB, m)
 }
 
 // 用主键(属性)做条件，更新除主键外的所有字段
-func (op *gamescorelockerOp) UpdateTx(ext sqlx.Ext, m *Gamescorelocker) (error) {
-    sql := `update gamescorelocker set KindID=?,ServerID=?,HallNodeID=?,GameNodeID=?,roomid=?,EnterIP=?,EnterMachine=?,CollectDate=? where UserID=?`
-    _, err := ext.Exec(sql,
-    m.KindID,
-        m.ServerID,
-        m.HallNodeID,
-        m.GameNodeID,
-        m.Roomid,
-        m.EnterIP,
-        m.EnterMachine,
-        m.CollectDate,
-        m.UserID,
-        )
+func (op *gamescorelockerOp) UpdateTx(ext sqlx.Ext, m *Gamescorelocker) error {
+	sql := `update gamescorelocker set KindID=?,ServerID=?,HallNodeID=?,GameNodeID=?,roomid=?,EnterIP=?,EnterMachine=?,CollectDate=? where UserID=?`
+	_, err := ext.Exec(sql,
+		m.KindID,
+		m.ServerID,
+		m.HallNodeID,
+		m.GameNodeID,
+		m.Roomid,
+		m.EnterIP,
+		m.EnterMachine,
+		m.CollectDate,
+		m.UserID,
+	)
 
-    if err != nil{
-		log.Error("update sql error:%v, data:%v", err.Error(),m)
-        return err
-    }
+	if err != nil {
+		log.Error("update sql error:%v, data:%v", err.Error(), m)
+		return err
+	}
 
-    return nil
+	return nil
 }
 
 // 用主键做条件，更新map里包含的字段名
-func (op *gamescorelockerOp) UpdateWithMap(UserID int, m map[string]interface{}) (error) {
-    return op.UpdateWithMapTx(db.DB, UserID, m)
+func (op *gamescorelockerOp) UpdateWithMap(UserID int, m map[string]interface{}) error {
+	return op.UpdateWithMapTx(db.DB, UserID, m)
 }
 
 // 用主键做条件，更新map里包含的字段名
-func (op *gamescorelockerOp) UpdateWithMapTx(ext sqlx.Ext, UserID int, m map[string]interface{}) (error) {
+func (op *gamescorelockerOp) UpdateWithMapTx(ext sqlx.Ext, UserID int, m map[string]interface{}) error {
 
-    sql := `update gamescorelocker set %s where 1=1 and UserID=? ;`
+	sql := `update gamescorelocker set %s where 1=1 and UserID=? ;`
 
-    var params []interface{}
-    var set_sql string
-    for k, v := range m{
+	var params []interface{}
+	var set_sql string
+	for k, v := range m {
 		if set_sql != "" {
 			set_sql += ","
 		}
-        set_sql += fmt.Sprintf(" %s=? ", k)
-        params = append(params, v)
-    }
+		set_sql += fmt.Sprintf(" %s=? ", k)
+		params = append(params, v)
+	}
 	params = append(params, UserID)
-    _, err := ext.Exec(fmt.Sprintf(sql, set_sql), params...)
-    return err
+	_, err := ext.Exec(fmt.Sprintf(sql, set_sql), params...)
+	return err
 }
 
 /*
@@ -191,54 +193,53 @@ func (i *Gamescorelocker) Delete() error{
 }
 */
 // 根据主键删除相关记录
-func (op *gamescorelockerOp) Delete(UserID int) error{
-    return op.DeleteTx(db.DB, UserID)
+func (op *gamescorelockerOp) Delete(UserID int) error {
+	return op.DeleteTx(db.DB, UserID)
 }
 
 // 根据主键删除相关记录,Tx
-func (op *gamescorelockerOp) DeleteTx(ext sqlx.Ext, UserID int) error{
-    sql := `delete from gamescorelocker where 1=1
+func (op *gamescorelockerOp) DeleteTx(ext sqlx.Ext, UserID int) error {
+	sql := `delete from gamescorelocker where 1=1
         and UserID=?
         `
-    _, err := ext.Exec(sql, 
-        UserID,
-        )
-    return err
+	_, err := ext.Exec(sql,
+		UserID,
+	)
+	return err
 }
 
 // 返回符合查询条件的记录数
 func (op *gamescorelockerOp) CountByMap(m map[string]interface{}) (int64, error) {
 
-    var params []interface{}
-    sql := `select count(*) from gamescorelocker where 1=1 `
-    for k, v := range m{
-        sql += fmt.Sprintf(" and  %s=? ",k)
-        params = append(params, v)
-    }
-    count := int64(-1)
-    err := db.DB.Get(&count, sql, params...)
-    if err != nil {
-        log.Error("CountByMap  error:%v data :%v", err.Error(), m)
-		return 0,err
-    }
-    return count, nil
+	var params []interface{}
+	sql := `select count(*) from gamescorelocker where 1=1 `
+	for k, v := range m {
+		sql += fmt.Sprintf(" and  %s=? ", k)
+		params = append(params, v)
+	}
+	count := int64(-1)
+	err := db.DB.Get(&count, sql, params...)
+	if err != nil {
+		log.Error("CountByMap  error:%v data :%v", err.Error(), m)
+		return 0, err
+	}
+	return count, nil
 }
 
-func (op *gamescorelockerOp) DeleteByMap(m map[string]interface{})(int64, error){
+func (op *gamescorelockerOp) DeleteByMap(m map[string]interface{}) (int64, error) {
 	return op.DeleteByMapTx(db.DB, m)
 }
 
-func (op *gamescorelockerOp) DeleteByMapTx(ext sqlx.Ext, m map[string]interface{}) (int64, error){
+func (op *gamescorelockerOp) DeleteByMapTx(ext sqlx.Ext, m map[string]interface{}) (int64, error) {
 	var params []interface{}
 	sql := "delete from gamescorelocker where 1=1 "
 	for k, v := range m {
 		sql += fmt.Sprintf(" and %s=? ", k)
 		params = append(params, v)
 	}
-	result, err := ext.Exec(sql, params...) 
-    if err != nil {
-        return -1, err
-    }
-    return result.RowsAffected()
+	result, err := ext.Exec(sql, params...)
+	if err != nil {
+		return -1, err
+	}
+	return result.RowsAffected()
 }
-
--- gameServer/db/model/token_record.go
+++ gameServer/db/model/token_record.go
@@ -1,11 +1,13 @@
 package model
 
-import(
-    "mj/gameServer/db"
-    "github.com/lovelly/leaf/log"
-    "github.com/jmoiron/sqlx"
-    "fmt"
-    "strings"
+import (
+	"errors"
+	"fmt"
+	"mj/gameServer/db"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/lovelly/leaf/log"
 )
 
 //This file is generate by scripts,don't edit it
@@ -15,36 +17,37 @@ import(
 
 // +gen *
 type TokenRecord struct {
-    RoomId int `db:"room_id" json:"room_id"` // 
-    UserId int `db:"user_id" json:"user_id"` // 
-    TokenType int `db:"tokenType" json:"tokenType"` // 
-    Amount int `db:"amount" json:"amount"` // 
-    Status int `db:"status" json:"status"` // 
-    CreatorTime *time.Time `db:"creator_time" json:"creator_time"` // 
-    KindID int `db:"KindID" json:"KindID"` // 
-    ServerId int `db:"ServerId" json:"ServerId"` // 
-    }
+	RoomId      int        `db:"room_id" json:"room_id"`           //
+	UserId      int        `db:"user_id" json:"user_id"`           //
+	TokenType   int        `db:"tokenType" json:"tokenType"`       //
+	Amount      int        `db:"amount" json:"amount"`             //
+	Status      int        `db:"status" json:"status"`             //
+	CreatorTime *time.Time `db:"creator_time" json:"creator_time"` //
+	KindID      int        `db:"KindID" json:"KindID"`             //
+	ServerId    int        `db:"ServerId" json:"ServerId"`         //
+}
 
 type tokenRecordOp struct{}
 
 var TokenRecordOp = &tokenRecordOp{}
 var DefaultTokenRecord = &TokenRecord{}
+
 // 按主键查询. 注:未找到记录的话将触发sql.ErrNoRows错误，返回nil, false
-func (op *tokenRecordOp) Get(room_id int,user_id int) (*TokenRecord, bool) {
-    obj := &TokenRecord{}
-    sql := "select * from token_record where room_id=? and user_id=? "
-    err := db.DB.Get(obj, sql, 
-        room_id,
-        user_id,
-        )
-    
-    if err != nil{
-        log.Error("Get data error:%v", err.Error())
-        return nil,false
-    }
-    return obj, true
-} 
-func(op *tokenRecordOp) SelectAll() ([]*TokenRecord, error) {
+func (op *tokenRecordOp) Get(room_id int, user_id int) (*TokenRecord, bool) {
+	obj := &TokenRecord{}
+	sql := "select * from token_record where room_id=? and user_id=? "
+	err := db.DB.Get(obj, sql,
+		room_id,
+		user_id,
+	)
+
+	if err != nil {
+		log.Error("Get data error:%v", err.Error())
+		return nil, false
+	}
+	return obj, true
+}
+func (op *tokenRecordOp) SelectAll() ([]*TokenRecord, error) {
 	objList := []*TokenRecord{}
 	sql := "select * from token_record "
 	err := db.DB.Select(&objList, sql)
@@ -55,15 +58,15 @@ func(op *tokenRecordOp) SelectAll() ([]*TokenRecord, error) {
 	return objList, nil
 }
 
-func(op *tokenRecordOp) QueryByMap(m map[string]interface{}) ([]*TokenRecord, error) {
+func (op *tokenRecordOp) QueryByMap(m map[string]interface{}) ([]*TokenRecord, error) {
 	result := []*TokenRecord{}
-    var params []interface{}
+	var params []interface{}
 
 	sql := "select * from token_record where 1=1 "
-    for k, v := range m{
-        sql += fmt.Sprintf(" and %s=? ", k)
-        params = append(params, v)
-    }
+	for k, v := range m {
+		sql += fmt.Sprintf(" and %s=? ", k)
+		params = append(params, v)
+	}
 	err := db.DB.Select(&result, sql, params...)
 	if err != nil {
 		log.Error(err.Error())
@@ -72,16 +75,15 @@ func(op *tokenRecordOp) QueryByMap(m map[string]interface{}) ([]*TokenRecord, er
 	return result, nil
 }
 
-
-func(op *tokenRecordOp) GetByMap(m map[string]interface{}) (*TokenRecord, error) {
-    lst, err := op.QueryByMap(m)
-    if err != nil {
-        return nil, err
-    }
-    if len(lst) > 0 {
-        return lst[0], nil
-    }
-    return nil, errors.New("no row in result")
+func (op *tokenRecordOp) GetByMap(m map[string]interface{}) (*TokenRecord, error) {
+	lst, err := op.QueryByMap(m)
+	if err != nil {
+		return nil, err
+	}
+	if len(lst) > 0 {
+		return lst[0], nil
+	}
+	return nil, errors.New("no row in result")
 }
 
 /*
@@ -96,29 +98,29 @@ func (i *TokenRecord) Insert() error {
 
 // 插入数据，自增长字段将被忽略
 func (op *tokenRecordOp) Insert(m *TokenRecord) (int64, error) {
-    return op.InsertTx(db.DB, m)
+	return op.InsertTx(db.DB, m)
 }
 
 // 插入数据，自增长字段将被忽略
 func (op *tokenRecordOp) InsertTx(ext sqlx.Ext, m *TokenRecord) (int64, error) {
-    sql := "insert into token_record(room_id,user_id,tokenType,amount,status,creator_time,KindID,ServerId) values(?,?,?,?,?,?,?,?)"
-    result, err := ext.Exec(sql,
-    m.RoomId,
-        m.UserId,
-        m.TokenType,
-        m.Amount,
-        m.Status,
-        m.CreatorTime,
-        m.KindID,
-        m.ServerId,
-        )
-    if err != nil{
-        log.Error("InsertTx sql error:%v, data:%v", err.Error(),m)
-        return -1, err
-    }
-    affected, _ := result.LastInsertId()
-        return affected, nil
-    }
+	sql := "insert into token_record(room_id,user_id,tokenType,amount,status,creator_time,KindID,ServerId) values(?,?,?,?,?,?,?,?)"
+	result, err := ext.Exec(sql,
+		m.RoomId,
+		m.UserId,
+		m.TokenType,
+		m.Amount,
+		m.Status,
+		m.CreatorTime,
+		m.KindID,
+		m.ServerId,
+	)
+	if err != nil {
+		log.Error("InsertTx sql error:%v, data:%v", err.Error(), m)
+		return -1, err
+	}
+	affected, _ := result.LastInsertId()
+	return affected, nil
+}
 
 /*
 func (i *TokenRecord) Update()  error {
@@ -131,54 +133,54 @@ func (i *TokenRecord) Update()  error {
 */
 
 // 用主键(属性)做条件，更新除主键外的所有字段
-func (op *tokenRecordOp) Update(m *TokenRecord) (error) {
-    return op.UpdateTx(db.DB, m)
+func (op *tokenRecordOp) Update(m *TokenRecord) error {
+	return op.UpdateTx(db.DB, m)
 }
 
 // 用主键(属性)做条件，更新除主键外的所有字段
-func (op *tokenRecordOp) UpdateTx(ext sqlx.Ext, m *TokenRecord) (error) {
-    sql := `update token_record set tokenType=?,amount=?,status=?,creator_time=?,KindID=?,ServerId=? where room_id=? and user_id=?`
-    _, err := ext.Exec(sql,
-    m.TokenType,
-        m.Amount,
-        m.Status,
-        m.CreatorTime,
-        m.KindID,
-        m.ServerId,
-        m.RoomId,
-        m.UserId,
-        )
+func (op *tokenRecordOp) UpdateTx(ext sqlx.Ext, m *TokenRecord) error {
+	sql := `update token_record set tokenType=?,amount=?,status=?,creator_time=?,KindID=?,ServerId=? where room_id=? and user_id=?`
+	_, err := ext.Exec(sql,
+		m.TokenType,
+		m.Amount,
+		m.Status,
+		m.CreatorTime,
+		m.KindID,
+		m.ServerId,
+		m.RoomId,
+		m.UserId,
+	)
 
-    if err != nil{
-		log.Error("update sql error:%v, data:%v", err.Error(),m)
-        return err
-    }
+	if err != nil {
+		log.Error("update sql error:%v, data:%v", err.Error(), m)
+		return err
+	}
 
-    return nil
+	return nil
 }
 
 // 用主键做条件，更新map里包含的字段名
-func (op *tokenRecordOp) UpdateWithMap(room_id int,user_id int, m map[string]interface{}) (error) {
-    return op.UpdateWithMapTx(db.DB, room_id,user_id, m)
+func (op *tokenRecordOp) UpdateWithMap(room_id int, user_id int, m map[string]interface{}) error {
+	return op.UpdateWithMapTx(db.DB, room_id, user_id, m)
 }
 
 // 用主键做条件，更新map里包含的字段名
-func (op *tokenRecordOp) UpdateWithMapTx(ext sqlx.Ext, room_id int,user_id int, m map[string]interface{}) (error) {
+func (op *tokenRecordOp) UpdateWithMapTx(ext sqlx.Ext, room_id int, user_id int, m map[string]interface{}) error {
 
-    sql := `update token_record set %s where 1=1 and room_id=? and user_id=? ;`
+	sql := `update token_record set %s where 1=1 and room_id=? and user_id=? ;`
 
-    var params []interface{}
-    var set_sql string
-    for k, v := range m{
+	var params []interface{}
+	var set_sql string
+	for k, v := range m {
 		if set_sql != "" {
 			set_sql += ","
 		}
-        set_sql += fmt.Sprintf(" %s=? ", k)
-        params = append(params, v)
-    }
-	params = append(params, room_id,user_id)
-    _, err := ext.Exec(fmt.Sprintf(sql, set_sql), params...)
-    return err
+		set_sql += fmt.Sprintf(" %s=? ", k)
+		params = append(params, v)
+	}
+	params = append(params, room_id, user_id)
+	_, err := ext.Exec(fmt.Sprintf(sql, set_sql), params...)
+	return err
 }
 
 /*
@@ -189,56 +191,55 @@ func (i *TokenRecord) Delete() error{
 }
 */
 // 根据主键删除相关记录
-func (op *tokenRecordOp) Delete(room_id int,user_id int) error{
-    return op.DeleteTx(db.DB, room_id,user_id)
+func (op *tokenRecordOp) Delete(room_id int, user_id int) error {
+	return op.DeleteTx(db.DB, room_id, user_id)
 }
 
 // 根据主键删除相关记录,Tx
-func (op *tokenRecordOp) DeleteTx(ext sqlx.Ext, room_id int,user_id int) error{
-    sql := `delete from token_record where 1=1
+func (op *tokenRecordOp) DeleteTx(ext sqlx.Ext, room_id int, user_id int) error {
+	sql := `delete from token_record where 1=1
         and room_id=?
         and user_id=?
         `
-    _, err := ext.Exec(sql, 
-        room_id,
-        user_id,
-        )
-    return err
+	_, err := ext.Exec(sql,
+		room_id,
+		user_id,
+	)
+	return err
 }
 
 // 返回符合查询条件的记录数
 func (op *tokenRecordOp) CountByMap(m map[string]interface{}) (int64, error) {
 
-    var params []interface{}
-    sql := `select count(*) from token_record where 1=1 `
-    for k, v := range m{
-        sql += fmt.Sprintf(" and  %s=? ",k)
-        params = append(params, v)
-    }
-    count := int64(-1)
-    err := db.DB.Get(&count, sql, params...)
-    if err != nil {
-        log.Error("CountByMap  error:%v data :%v", err.Error(), m)
-		return 0,err
-    }
-    return count, nil
+	var params []interface{}
+	sql := `select count(*) from token_record where 1=1 `
+	for k, v := range m {
+		sql += fmt.Sprintf(" and  %s=? ", k)
+		params = append(params, v)
+	}
+	count := int64(-1)
+	err := db.DB.Get(&count, sql, params...)
+	if err != nil {
+		log.Error("CountByMap  error:%v data :%v", err.Error(), m)
+		return 0, err
+	}
+	return count, nil
 }
 
-func (op *tokenRecordOp) DeleteByMap(m map[string]interface{})(int64, error){
+func (op *tokenRecordOp) DeleteByMap(m map[string]interface{}) (int64, error) {
 	return op.DeleteByMapTx(db.DB, m)
 }
 
-func (op *tokenRecordOp) DeleteByMapTx(ext sqlx.Ext, m map[string]interface{}) (int64, error){
+func (op *tokenRecordOp) DeleteByMapTx(ext sqlx.Ext, m map[string]interface{}) (int64, error) {
 	var params []interface{}
 	sql := "delete from token_record where 1=1 "
 	for k, v := range m {
 		sql += fmt.Sprintf(" and %s=? ", k)
 		params = append(params, v)
 	}
-	result, err := ext.Exec(sql, params...) 
-    if err != nil {
-        return -1, err
-    }
-    return result.RowsAffected()
+	result, err := ext.Exec(sql, params...)
+	if err != nil {
+		return -1, err
+	}
+	return result.RowsAffected()
 }
-
