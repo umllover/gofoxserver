a547072d11141610b9f05cf8d6bae969d9fdc47a
jiaowen <jiaowen@mq.com>
2017-07-25 13:00:06 +0800

    update

--- gameServer/db/model/base/table_map.go
+++ gameServer/db/model/base/table_map.go
@@ -6,6 +6,7 @@ package base
 func InitTableMap() {
 	/*
 	   db.BaseDBMap.AddTableWithName(Activity{}, "activity").SetKeys(false,"Id")
+	   db.BaseDBMap.AddTableWithName(FreeLimit{}, "free_limit").SetKeys(false,"FreeName")
 	   db.BaseDBMap.AddTableWithName(GameServiceOption{}, "game_service_option").SetKeys(false,"KindID","ServerID")
 	   db.BaseDBMap.AddTableWithName(GameTestpai{}, "game_testpai").SetKeys(true, "Id")
 	   db.BaseDBMap.AddTableWithName(GlobalVar{}, "global_var").SetKeys(false,"K")
