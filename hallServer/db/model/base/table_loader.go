0ac1053c6ccb5121e8cb21157df0e365dbc97845
laiwenlong <laiwenlong@mq.com>
2017-07-28 11:49:24 +0800

    数据库更新

--- hallServer/db/model/base/table_loader.go
+++ hallServer/db/model/base/table_loader.go
@@ -5,30 +5,32 @@ package base
 
 import (
 	"mj/hallServer/db"
-    "github.com/lovelly/leaf/log"
+	"time"
+
+	"github.com/lovelly/leaf/log"
 )
 
 func LoadBaseData() {
 	var start = time.Now().UnixNano()
-    ActivityCache.LoadAll()
-    FreeLimitCache.LoadAll()
-    GameServiceOptionCache.LoadAll()
-    GameTestpaiCache.LoadAll()
-    GlobalVarCache.LoadAll()
-    GoodsCache.LoadAll()
-    PersonalTableFeeCache.LoadAll()
-    PersonalTableFeeBak-7-28Cache.LoadAll()
-    RefreshInTimeCache.LoadAll()
-    ServerListCache.LoadAll()
-    db.BaseDataCaches["Activity"] = ActivityCache
-    db.BaseDataCaches["FreeLimit"] = FreeLimitCache
-    db.BaseDataCaches["GameServiceOption"] = GameServiceOptionCache
-    db.BaseDataCaches["GameTestpai"] = GameTestpaiCache
-    db.BaseDataCaches["GlobalVar"] = GlobalVarCache
-    db.BaseDataCaches["Goods"] = GoodsCache
-    db.BaseDataCaches["PersonalTableFee"] = PersonalTableFeeCache
-    db.BaseDataCaches["PersonalTableFeeBak-7-28"] = PersonalTableFeeBak-7-28Cache
-    db.BaseDataCaches["RefreshInTime"] = RefreshInTimeCache
-    db.BaseDataCaches["ServerList"] = ServerListCache
-    log.Debug("loadBaseData %v  %v %v", 10,  time.Now().UnixNano()-start, "ns")
+	ActivityCache.LoadAll()
+	FreeLimitCache.LoadAll()
+	GameServiceOptionCache.LoadAll()
+	GameTestpaiCache.LoadAll()
+	GlobalVarCache.LoadAll()
+	GoodsCache.LoadAll()
+	PersonalTableFeeCache.LoadAll()
+	PersonalTableFeeBak728Cache.LoadAll()
+	RefreshInTimeCache.LoadAll()
+	ServerListCache.LoadAll()
+	db.BaseDataCaches["Activity"] = ActivityCache
+	db.BaseDataCaches["FreeLimit"] = FreeLimitCache
+	db.BaseDataCaches["GameServiceOption"] = GameServiceOptionCache
+	db.BaseDataCaches["GameTestpai"] = GameTestpaiCache
+	db.BaseDataCaches["GlobalVar"] = GlobalVarCache
+	db.BaseDataCaches["Goods"] = GoodsCache
+	db.BaseDataCaches["PersonalTableFee"] = PersonalTableFeeCache
+	db.BaseDataCaches["PersonalTableFeeBak728"] = PersonalTableFeeBak728Cache
+	db.BaseDataCaches["RefreshInTime"] = RefreshInTimeCache
+	db.BaseDataCaches["ServerList"] = ServerListCache
+	log.Debug("loadBaseData %v  %v %v", 10, time.Now().UnixNano()-start, "ns")
 }
