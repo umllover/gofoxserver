5338735052595e17c585c2c9cacdca33879412cb
huangkunbin <huangkunbin@gmail.com>
2017-08-01 11:59:49 +0800

    update

---
+++ gameServer/db/model/base/agent_limit.go
@@ -0,0 +1,60 @@
+package base
+
+import (
+	"mj/gameServer/db"
+
+	"github.com/lovelly/leaf/log"
+)
+
+//This file is generate by scripts,don't edit it
+
+//agent_limit
+//
+
+// +gen
+type AgentLimit struct {
+	Level  int    `db:"level" json:"level"`   //
+	Min    int    `db:"min" json:"min"`       //
+	Max    int    `db:"max" json:"max"`       //
+	Remark string `db:"remark" json:"remark"` //
+}
+
+var DefaultAgentLimit = AgentLimit{}
+
+type agentLimitCache struct {
+	objMap  map[int]*AgentLimit
+	objList []*AgentLimit
+}
+
+var AgentLimitCache = &agentLimitCache{}
+
+func (c *agentLimitCache) LoadAll() {
+	sql := "select * from agent_limit"
+	c.objList = make([]*AgentLimit, 0)
+	err := db.BaseDB.Select(&c.objList, sql)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	c.objMap = make(map[int]*AgentLimit)
+	log.Debug("Load all agent_limit success %v", len(c.objList))
+	for _, v := range c.objList {
+		c.objMap[v.Level] = v
+	}
+}
+
+func (c *agentLimitCache) All() []*AgentLimit {
+	return c.objList
+}
+
+func (c *agentLimitCache) Count() int {
+	return len(c.objList)
+}
+
+func (c *agentLimitCache) Get(level int) (*AgentLimit, bool) {
+	return c.GetKey1(level)
+}
+
+func (c *agentLimitCache) GetKey1(level int) (*AgentLimit, bool) {
+	v, ok := c.objMap[level]
+	return v, ok
+}
--- gameServer/db/model/stats/getin_room_log.go
+++ gameServer/db/model/stats/getin_room_log.go
@@ -17,13 +17,20 @@ import (
 
 // +gen *
 type GetinRoomLog struct {
-	GetinRecodeId int        `db:"getin_recode_id" json:"getin_recode_id"` // 加入游戏数据记录的Id
-	RoomId        int        `db:"room_id" json:"room_id"`                 // 房间id
-	UserId        int64      `db:"user_id" json:"user_id"`                 // 用户索引
-	PublicGetin   int        `db:"public_getIn" json:"public_getIn"`       // 公房加入 0否 1是
-	PrivateGetin  int        `db:"private_getIn" json:"private_getIn"`     // 私房加入 0否 1是
-	TypeGetin     int        `db:"type_getIn" json:"type_getIn"`           // 加入房间类型 0列表加入 2输房号加入 3快速加入 4点击链接加入
-	GetInTime     *time.Time `db:"getIn_time" json:"getIn_time"`           // 进入房间时间
+	RecodeId     int        `db:"recode_id" json:"recode_id"`           // 加入游戏数据记录的Id
+	RoomId       int        `db:"room_id" json:"room_id"`               // 房间id
+	UserId       int64      `db:"user_id" json:"user_id"`               // 用户索引
+	KindId       int        `db:"kind_id" json:"kind_id"`               // 房间索引
+	ServiceId    int        `db:"service_id" json:"service_id"`         // 游戏标识
+	RoomName     string     `db:"room_name" json:"room_name"`           //
+	NodeId       int        `db:"node_id" json:"node_id"`               // 在哪个服务器上
+	Num          int        `db:"num" json:"num"`                       // 局数
+	Status       int        `db:"status" json:"status"`                 //
+	Public       int        `db:"public" json:"public"`                 // 公房加入 0否 1是
+	MaxPlayerCnt int        `db:"max_player_cnt" json:"max_player_cnt"` // 最多几个玩家进入
+	PayType      int        `db:"pay_type" json:"pay_type"`             // 支付方式 1是全服 2是AA
+	TypeGetin    int        `db:"type_getIn" json:"type_getIn"`         // 加入房间类型 0列表加入 2输房号加入 3快速加入 4点击链接加入
+	GetInTime    *time.Time `db:"getIn_time" json:"getIn_time"`         // 进入房间时间
 }
 
 type getinRoomLogOp struct{}
@@ -32,11 +39,11 @@ var GetinRoomLogOp = &getinRoomLogOp{}
 var DefaultGetinRoomLog = &GetinRoomLog{}
 
 // 按主键查询. 注:未找到记录的话将触发sql.ErrNoRows错误，返回nil, false
-func (op *getinRoomLogOp) Get(getin_recode_id int) (*GetinRoomLog, bool) {
+func (op *getinRoomLogOp) Get(recode_id int) (*GetinRoomLog, bool) {
 	obj := &GetinRoomLog{}
-	sql := "select * from getin_room_log where getin_recode_id=? "
+	sql := "select * from getin_room_log where recode_id=? "
 	err := db.StatsDB.Get(obj, sql,
-		getin_recode_id,
+		recode_id,
 	)
 
 	if err != nil {
@@ -101,12 +108,19 @@ func (op *getinRoomLogOp) Insert(m *GetinRoomLog) (int64, error) {
 
 // 插入数据，自增长字段将被忽略
 func (op *getinRoomLogOp) InsertTx(ext sqlx.Ext, m *GetinRoomLog) (int64, error) {
-	sql := "insert into getin_room_log(room_id,user_id,public_getIn,private_getIn,type_getIn,getIn_time) values(?,?,?,?,?,?)"
+	sql := "insert into getin_room_log(room_id,user_id,kind_id,service_id,room_name,node_id,num,status,public,max_player_cnt,pay_type,type_getIn,getIn_time) values(?,?,?,?,?,?,?,?,?,?,?,?,?)"
 	result, err := ext.Exec(sql,
 		m.RoomId,
 		m.UserId,
-		m.PublicGetin,
-		m.PrivateGetin,
+		m.KindId,
+		m.ServiceId,
+		m.RoomName,
+		m.NodeId,
+		m.Num,
+		m.Status,
+		m.Public,
+		m.MaxPlayerCnt,
+		m.PayType,
 		m.TypeGetin,
 		m.GetInTime,
 	)
@@ -120,11 +134,18 @@ func (op *getinRoomLogOp) InsertTx(ext sqlx.Ext, m *GetinRoomLog) (int64, error)
 
 //存在就更新， 不存在就插入
 func (op *getinRoomLogOp) InsertUpdate(obj *GetinRoomLog, m map[string]interface{}) error {
-	sql := "insert into getin_room_log(room_id,user_id,public_getIn,private_getIn,type_getIn,getIn_time) values(?,?,?,?,?,?) ON DUPLICATE KEY UPDATE "
+	sql := "insert into getin_room_log(room_id,user_id,kind_id,service_id,room_name,node_id,num,status,public,max_player_cnt,pay_type,type_getIn,getIn_time) values(?,?,?,?,?,?,?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE "
 	var params = []interface{}{obj.RoomId,
 		obj.UserId,
-		obj.PublicGetin,
-		obj.PrivateGetin,
+		obj.KindId,
+		obj.ServiceId,
+		obj.RoomName,
+		obj.NodeId,
+		obj.Num,
+		obj.Status,
+		obj.Public,
+		obj.MaxPlayerCnt,
+		obj.PayType,
 		obj.TypeGetin,
 		obj.GetInTime,
 	}
@@ -158,15 +179,22 @@ func (op *getinRoomLogOp) Update(m *GetinRoomLog) error {
 
 // 用主键(属性)做条件，更新除主键外的所有字段
 func (op *getinRoomLogOp) UpdateTx(ext sqlx.Ext, m *GetinRoomLog) error {
-	sql := `update getin_room_log set room_id=?,user_id=?,public_getIn=?,private_getIn=?,type_getIn=?,getIn_time=? where getin_recode_id=?`
+	sql := `update getin_room_log set room_id=?,user_id=?,kind_id=?,service_id=?,room_name=?,node_id=?,num=?,status=?,public=?,max_player_cnt=?,pay_type=?,type_getIn=?,getIn_time=? where recode_id=?`
 	_, err := ext.Exec(sql,
 		m.RoomId,
 		m.UserId,
-		m.PublicGetin,
-		m.PrivateGetin,
+		m.KindId,
+		m.ServiceId,
+		m.RoomName,
+		m.NodeId,
+		m.Num,
+		m.Status,
+		m.Public,
+		m.MaxPlayerCnt,
+		m.PayType,
 		m.TypeGetin,
 		m.GetInTime,
-		m.GetinRecodeId,
+		m.RecodeId,
 	)
 
 	if err != nil {
@@ -178,14 +206,14 @@ func (op *getinRoomLogOp) UpdateTx(ext sqlx.Ext, m *GetinRoomLog) error {
 }
 
 // 用主键做条件，更新map里包含的字段名
-func (op *getinRoomLogOp) UpdateWithMap(getin_recode_id int, m map[string]interface{}) error {
-	return op.UpdateWithMapTx(db.StatsDB, getin_recode_id, m)
+func (op *getinRoomLogOp) UpdateWithMap(recode_id int, m map[string]interface{}) error {
+	return op.UpdateWithMapTx(db.StatsDB, recode_id, m)
 }
 
 // 用主键做条件，更新map里包含的字段名
-func (op *getinRoomLogOp) UpdateWithMapTx(ext sqlx.Ext, getin_recode_id int, m map[string]interface{}) error {
+func (op *getinRoomLogOp) UpdateWithMapTx(ext sqlx.Ext, recode_id int, m map[string]interface{}) error {
 
-	sql := `update getin_room_log set %s where 1=1 and getin_recode_id=? ;`
+	sql := `update getin_room_log set %s where 1=1 and recode_id=? ;`
 
 	var params []interface{}
 	var set_sql string
@@ -196,7 +224,7 @@ func (op *getinRoomLogOp) UpdateWithMapTx(ext sqlx.Ext, getin_recode_id int, m m
 		set_sql += fmt.Sprintf(" %s=? ", k)
 		params = append(params, v)
 	}
-	params = append(params, getin_recode_id)
+	params = append(params, recode_id)
 	_, err := ext.Exec(fmt.Sprintf(sql, set_sql), params...)
 	return err
 }
@@ -209,17 +237,17 @@ func (i *GetinRoomLog) Delete() error{
 }
 */
 // 根据主键删除相关记录
-func (op *getinRoomLogOp) Delete(getin_recode_id int) error {
-	return op.DeleteTx(db.StatsDB, getin_recode_id)
+func (op *getinRoomLogOp) Delete(recode_id int) error {
+	return op.DeleteTx(db.StatsDB, recode_id)
 }
 
 // 根据主键删除相关记录,Tx
-func (op *getinRoomLogOp) DeleteTx(ext sqlx.Ext, getin_recode_id int) error {
+func (op *getinRoomLogOp) DeleteTx(ext sqlx.Ext, recode_id int) error {
 	sql := `delete from getin_room_log where 1=1
-        and getin_recode_id=?
+        and recode_id=?
         `
 	_, err := ext.Exec(sql,
-		getin_recode_id,
+		recode_id,
 	)
 	return err
 }
