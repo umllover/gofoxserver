7bce50ef817b78d99ff7ee74c9d0a9ded3ca37cf
huangkunbin <huangkunbin@gmail.com>
2017-08-02 11:13:59 +0800

    update

--- gameServer/db/model/base/table_map.go
+++ gameServer/db/model/base/table_map.go
@@ -5,18 +5,18 @@ package base
 
 func InitTableMap() {
 	/*
-	   db.BaseDBMap.AddTableWithName(AchievementLimit{}, "achievement_limit").SetKeys(false,"Level")
 	   db.BaseDBMap.AddTableWithName(Activity{}, "activity").SetKeys(false,"Id")
-	   db.BaseDBMap.AddTableWithName(AgentLimit{}, "agent_limit").SetKeys(false,"Level")
 	   db.BaseDBMap.AddTableWithName(FreeLimit{}, "free_limit").SetKeys(false,"FreeName")
 	   db.BaseDBMap.AddTableWithName(GameServiceOption{}, "game_service_option").SetKeys(false,"KindID","ServerID")
 	   db.BaseDBMap.AddTableWithName(GameTestpai{}, "game_testpai").SetKeys(true, "Id")
 	   db.BaseDBMap.AddTableWithName(GlobalVar{}, "global_var").SetKeys(false,"K")
 	   db.BaseDBMap.AddTableWithName(Goods{}, "goods").SetKeys(false,"GoodsId")
+	   db.BaseDBMap.AddTableWithName(IncAgentNum{}, "inc_agent_num").SetKeys(false,"NodeId")
 	   db.BaseDBMap.AddTableWithName(PersonalTableFee{}, "personal_table_fee").SetKeys(false,"KindID","ServerID","DrawCountLimit")
 	   db.BaseDBMap.AddTableWithName(PersonalTableFeeBak728{}, "personal_table_fee_bak_7_28").SetKeys(false,"KindID","ServerID","DrawCountLimit")
-	   db.BaseDBMap.AddTableWithName(RechargeLimit{}, "recharge_limit").SetKeys(false,"Level")
 	   db.BaseDBMap.AddTableWithName(RefreshInTime{}, "refresh_in_time").SetKeys(false,"Id")
 	   db.BaseDBMap.AddTableWithName(ServerList{}, "server_list").SetKeys(false,"SvrId","SvrType")
+	   db.BaseDBMap.AddTableWithName(Upgrade{}, "upgrade").SetKeys(false,"LevelId")
+	   db.BaseDBMap.AddTableWithName(UpgradeAdvisor{}, "upgrade_advisor").SetKeys(false,"AdvisorId")
 	*/
 }
