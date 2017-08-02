21e7c003b561cccd28ab1a80b5f3d710a2cb2cb4
laiwenlong <laiwenlong@mq.com>
2017-08-02 17:34:16 +0800

    服务器断开连接，通知客户端

--- gameServer/userHandle/external.go
+++ gameServer/userHandle/external.go
@@ -1,10 +1,13 @@
 package userHandle
 
 import (
-	"mj/gameServer/userHandle/internal"
 	"mj/gameServer/user"
+	"mj/gameServer/userHandle/internal"
+
+	"time"
 
 	"github.com/lovelly/leaf/gate"
+	"github.com/lovelly/leaf/log"
 )
 
 var (
@@ -17,4 +20,12 @@ func NewUserHandle(a gate.Agent) gate.UserHandler {
 
 func ForEachUser(f func(u *user.User)) {
 	internal.ForEachUser(f)
+}
+
+func KickOutUser() {
+	log.Debug("at gameServer close, KickOutUser")
+	ForEachUser(func(player *user.User) {
+		player.ChanRPC().Go("SvrShutdown")
+	})
+	time.Sleep(5 * time.Second)
 }
