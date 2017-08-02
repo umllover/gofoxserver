96761c1c5513e36e2dc51aaf883c876d050b2adf
huangkunbin <huangkunbin@gmail.com>
2017-08-02 11:14:21 +0800

    update

--- hallServer/db/model/base/table_loader.go
+++ hallServer/db/model/base/table_loader.go
@@ -12,31 +12,31 @@ import (
 
 func LoadBaseData() {
 	var start = time.Now().UnixNano()
-	AchievementLimitCache.LoadAll()
 	ActivityCache.LoadAll()
-	AgentLimitCache.LoadAll()
 	FreeLimitCache.LoadAll()
 	GameServiceOptionCache.LoadAll()
 	GameTestpaiCache.LoadAll()
 	GlobalVarCache.LoadAll()
 	GoodsCache.LoadAll()
+	IncAgentNumCache.LoadAll()
 	PersonalTableFeeCache.LoadAll()
 	PersonalTableFeeBak728Cache.LoadAll()
-	RechargeLimitCache.LoadAll()
 	RefreshInTimeCache.LoadAll()
 	ServerListCache.LoadAll()
-	db.BaseDataCaches["AchievementLimit"] = AchievementLimitCache
+	UpgradeCache.LoadAll()
+	UpgradeAdvisorCache.LoadAll()
 	db.BaseDataCaches["Activity"] = ActivityCache
-	db.BaseDataCaches["AgentLimit"] = AgentLimitCache
 	db.BaseDataCaches["FreeLimit"] = FreeLimitCache
 	db.BaseDataCaches["GameServiceOption"] = GameServiceOptionCache
 	db.BaseDataCaches["GameTestpai"] = GameTestpaiCache
 	db.BaseDataCaches["GlobalVar"] = GlobalVarCache
 	db.BaseDataCaches["Goods"] = GoodsCache
+	db.BaseDataCaches["IncAgentNum"] = IncAgentNumCache
 	db.BaseDataCaches["PersonalTableFee"] = PersonalTableFeeCache
 	db.BaseDataCaches["PersonalTableFeeBak728"] = PersonalTableFeeBak728Cache
-	db.BaseDataCaches["RechargeLimit"] = RechargeLimitCache
 	db.BaseDataCaches["RefreshInTime"] = RefreshInTimeCache
 	db.BaseDataCaches["ServerList"] = ServerListCache
+	db.BaseDataCaches["Upgrade"] = UpgradeCache
+	db.BaseDataCaches["UpgradeAdvisor"] = UpgradeAdvisorCache
 	log.Debug("loadBaseData %v  %v %v", 13, time.Now().UnixNano()-start, "ns")
 }
