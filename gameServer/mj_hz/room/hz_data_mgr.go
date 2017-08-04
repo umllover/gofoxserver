da44175ed9e32f8a8eebff402c01e08a56a2a2d3
laiwenlong <laiwenlong@mq.com>
2017-08-04 11:39:24 +0800

    红中扎码

--- gameServer/mj_hz/room/hz_data_mgr.go
+++ gameServer/mj_hz/room/hz_data_mgr.go
@@ -5,11 +5,25 @@ import (
 	. "mj/gameServer/common/mj"
 	"mj/gameServer/common/mj/mj_base"
 	"mj/gameServer/db/model/base"
+
+	"github.com/lovelly/leaf/log"
 )
 
 func NewHZDataMgr(id int, uid int64, configIdx int, name string, temp *base.GameServiceOption, base *hz_entry, info *msg.L2G_CreatorRoom) *hz_data {
 	d := new(hz_data)
 	d.RoomData = mj_base.NewDataMgr(id, uid, configIdx, name, temp, base.Mj_base, info.OtherInfo)
+
+	getData, ok := d.OtherInfo["zhaMa"].(float64)
+	if !ok {
+		log.Error("hzmj at NewDataMgr [zhaMa] error")
+		return nil
+	}
+
+	//TODO 客户端发的个数有误，暂时强制改掉
+	getData = 2
+
+	d.ZhuaHuaCnt = int(getData)
+
 	return d
 }
 
