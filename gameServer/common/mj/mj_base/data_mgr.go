50208a0a0d2434aa481df5fd9d80d27b4c67bf7a
longhu <longhu@mq.com>
2017-08-07 17:35:11 +0800

    手牌设置数目出错

--- gameServer/common/mj/mj_base/data_mgr.go
+++ gameServer/common/mj/mj_base/data_mgr.go
@@ -1035,9 +1035,18 @@ func (room *RoomData) RepalceCard() {
 				break
 			}
 
+			max := room.GetCfg().MaxCount
+			mim := room.GetCfg().MaxCount - 1
 			testCards := make([]int, 0)
-			for idx, _ := range chairIds {
+			for idx, value := range chairIds {
 				card := utils.GetStrIntSixteenList(cards[idx], "，")
+				if value == 0 && max < len(card) {
+ 					log.Error("给庄家设置的手牌数目过多")
+					return
+				} else if mim < len(card) {
+					log.Error("给其他用户设置的手牌数目过多")
+					return
+				}
 				testCards = append(testCards, card...)
 			}
 			isRight := room.CheckUserCard(v.KindID, testCards)
