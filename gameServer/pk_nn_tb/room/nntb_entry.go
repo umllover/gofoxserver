521d4516f1e90adcc1a364fcc72f8a1362e22a1a
zhangdengqi <9682950@qq.com>
2017-08-09 10:46:23 +0800

    update

--- gameServer/pk_nn_tb/room/nntb_entry.go
+++ gameServer/pk_nn_tb/room/nntb_entry.go
@@ -3,6 +3,8 @@ package room
 import (
 	"mj/common/msg"
 	"mj/gameServer/common/pk/pk_base"
+	"mj/common/msg/nn_tb_msg"
+	"mj/gameServer/user"
 )
 
 func NewNNTBEntry(info *msg.L2G_CreatorRoom) *NNTB_Entry {
@@ -15,3 +17,34 @@ func NewNNTBEntry(info *msg.L2G_CreatorRoom) *NNTB_Entry {
 type NNTB_Entry struct {
 	*pk_base.Entry_base
 }
+
+
+
+
+//叫分(倍数)
+func (room *NNTB_Entry) CallScore(args []interface{}) {
+	recvMsg := args[0].(*nn_tb_msg.C2G_TBNN_CallScore)
+	u := args[1].(*user.User)
+
+	room.DataMgr.CallScore(u, recvMsg.CallScore)
+	return
+}
+
+//加注
+func (r *NNTB_Entry) AddScore(args []interface{}) {
+	recvMsg := args[0].(*nn_tb_msg.C2G_TBNN_AddScore)
+	u := args[1].(*user.User)
+
+	r.DataMgr.AddScore(u, recvMsg.Score)
+	return
+}
+
+// 亮牌
+func (r *NNTB_Entry) OpenCard(args []interface{}) {
+	recvMsg := args[0].(*nn_tb_msg.C2G_TBNN_OpenCard)
+	u := args[1].(*user.User)
+
+	r.DataMgr.OpenCard(u, recvMsg.CardType, recvMsg.CardData)
+	return
+}
+
