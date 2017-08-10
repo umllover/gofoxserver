39f7262dde7e6770ffcdc12955825d4057111a77
zhangdengqi <9682950@qq.com>
2017-08-10 10:09:10 +0800

    update

--- common/msg/nn_tb_msg/msg.go
+++ common/msg/nn_tb_msg/msg.go
@@ -6,10 +6,6 @@ import (
 
 func init() {
 	//-----------s2c-----------------
-	msg.Processor.Register(&G2C_TBNN_StatusFree{})
-	msg.Processor.Register(&G2C_TBNN_StatusCall{})
-	msg.Processor.Register(&G2C_TBNN_StatusScore{})
-	msg.Processor.Register(&G2C_TBNN_StatusPlay{})
 	msg.Processor.Register(&G2C_TBNN_CallScoreEnd{})
 	msg.Processor.Register(&G2C_TBNN_GameStart{})
 	msg.Processor.Register(&G2C_TBNN_GameEnd{})
@@ -29,76 +25,7 @@ func init() {
 	msg.Processor.Register(&C2G_TBNN_OpenCard{})
 }
 
-// ------------ s2c ----------------
-//---------- 游戏状态-------
-type G2C_TBNN_StatusFree struct {
-	CellScore int //基础积分
-
-	TurnScore    []int  //积分信息
-	CollectScore []int  //积分信息
-	GameRoomName string //房间名称
-
-	TimeOutCard     int   //出牌时间
-	TimeOperateCard int   //操作时间
-	TimeStartGame   int64 //开始时间
-
-	TimesCount int //倍数
-	PlayMode   int //游戏模式
-	CountLimit int //局数限制
-
-	PlayerCount      int     // 游戏人数
-	CurrentPlayCount int     //房间已玩局数
-	EachRoundScore   [][]int //房间每局游戏比分
-	InitScore        []int   //积分信息
-}
-
-type G2C_TBNN_StatusCall struct {
-	CallBanker  int   //叫庄用户
-	DynamicJoin int   //动态加入
-	PlayStatus  []int //用户状态
-
-	//历史积分
-	TurnScore    []int64 //积分信息
-	CollectScore []int64 //积分信息
-	GameRoomName string  //房间名称
-}
-
-type G2C_TBNN_StatusScore struct {
-	//下注信息
-	PlayStatusi  []int   //用户状态
-	DynamicJoin  int     //动态加入
-	TurnMaxScore int64   //最大下注
-	TableScore   []int64 //下注数目
-	BankerUser   int     //庄家用户
-	TurnScore    []int64 //积分信息
-	CollectScore []int64 //积分信息
-	GameRoomName string  //房间名称
-}
-
-type UserReLoginInfo struct {
-	ChairID        int
-	UserGameStatus int
-	CallScoreTimes int
-	AddScoreTimes  int
-	//OpenCardData []
-}
-type G2C_TBNN_StatusPlay struct {
-	CellScore int //基础积分
-
-	UserReLoginInfos []*UserReLoginInfo
-	GameStatus       int //游戏状态
-	PlayerCount      int //玩家人数
-	BankerUser       int //庄家用户
-	PublicCardData   []int
-	HandCardData     [][]int //桌面扑克
-
-	InitScore    []int  //积分信息
-	GameRoomName string //房间名称
-
-	CurrentPlayCount int //房间已玩局数
-	LimitPlayCount   int //总局数
-}
-
+// ------------ g2c ----------------
 //叫分结果
 type G2C_TBNN_CallScoreEnd struct {
 	Banker         int   //庄家用户
