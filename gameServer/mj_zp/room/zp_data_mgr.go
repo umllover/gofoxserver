ea74bd918b56d7d39081da88a56f6079ef2ba4b5
laiwenlong <laiwenlong@mq.com>
2017-08-15 15:39:17 +0800

    结束时出的牌重置

--- gameServer/mj_zp/room/zp_data_mgr.go
+++ gameServer/mj_zp/room/zp_data_mgr.go
@@ -390,14 +390,12 @@ func (room *ZP_RoomData) StartDispatchCard() {
 	//temp[3] = 4 //三张四同
 	//temp[4] = 3 //三张五同
 	//temp[5] = 1
-	//
 	////room.FlowerCnt[0] = 1 //花牌
 	//room.SendCardData = 0x04
 	//room.CardIndex[0] = temp
 	//GetCardWordArray(room.CardIndex[0])
 	//log.Debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 	//log.Debug("room.CardIndex:%v", room.CardIndex[0])
-	//
 	//for k := range room.RepertoryCard {
 	//	room.RepertoryCard[k] = 0x01
 	//}
@@ -2184,6 +2182,7 @@ func (room *ZP_RoomData) ClearAllTimer() {
 			room.OperateTime[k].Stop()
 		}
 	}
+	room.OutCardData = 0
 	log.Debug("zpmj at ClearAllTimer")
 }
 
