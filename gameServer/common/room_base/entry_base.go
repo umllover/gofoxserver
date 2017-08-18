057288d1457025a01230b3b5e35f3fe4e96c96be
laiwenlong <laiwenlong@mq.com>
2017-08-18 15:23:45 +0800

    退出房间逻辑修改

--- gameServer/common/room_base/entry_base.go
+++ gameServer/common/room_base/entry_base.go
@@ -392,19 +392,19 @@ func (room *Entry_base) OnRecUserTrustee(args []interface{}) {
 //玩家离开房间
 func (room *Entry_base) ReqLeaveRoom(args []interface{}) (interface{}, error) {
 	player := args[0].(*user.User)
-	if room.Status == RoomStatusReady {
-		if room.UserMgr.LeaveRoom(player, room.Status) {
-			player.WriteMsg(&msg.G2C_LeaveRoomRsp{Status: room.Status})
-		} else {
-			player.WriteMsg(&msg.G2C_LeaveRoomRsp{Status: room.Status, Code: ErrLoveRoomFaild})
-		}
-	} else {
+	if room.Status == RoomStatusStarting {
 		room.UserMgr.AddLeavePly(player.Id)
 		room.UserMgr.SendMsgAllNoSelf(player.Id, &msg.G2C_LeaveRoomBradcast{UserID: player.Id})
 		room.TimerMgr.StartReplytIimer(player.Id, func() {
 			player.WriteMsg(&msg.G2C_LeaveRoomRsp{Status: room.Status})
 			room.OnEventGameConclude(USER_LEAVE)
 		})
+	} else {
+		if room.UserMgr.LeaveRoom(player, room.Status) {
+			player.WriteMsg(&msg.G2C_LeaveRoomRsp{Status: room.Status})
+		} else {
+			player.WriteMsg(&msg.G2C_LeaveRoomRsp{Status: room.Status, Code: ErrLoveRoomFaild})
+		}
 	}
 	return nil, nil
 }
